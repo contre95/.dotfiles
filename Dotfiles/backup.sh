913da8095abc7824a823a2496b07a84427db2c57
Lucas <lucascontre95@gmail.com>
2020-05-17 15:55:21 +0000

    asd

---
+++ Dotfiles/backup.sh
@@ -0,0 +1,24 @@
+# i3 
+cp ~/.config/i3/config $MY_FOLDER/Dotfiles/.config/i3/config
+cp ~/.config/i3blocks/config $MY_FOLDER/Dotfiles/.config/i3blocks/config
+
+# Termite
+cp ~/.config/termite/config $MY_FOLDER/Dotfiles/.config/termite/config
+
+# Tmux
+cp ~/.tmux.conf $MY_FOLDER/Dotfiles/.tmux.conf
+
+# GTK3
+cp ~/.config/gtk-3.0/settings.ini $MY_FOLDER/Dotfiles/.config/gtk-3.0/settings.ini
+
+# Ranger
+cp -r ~/.config/ranger $MY_FOLDER/Dotfiles/.config/ranger
+
+# Vim 
+cp -r ~/.vim/colors $MY_FOLDER/Dotfiles/.vim/colors
+cp -r ~/.vim/syntax $MY_FOLDER/Dotfiles/.vim/syntax
+cp ~/.vimrc $MY_FOLDER/Dotfiles/.vimrc
+
+# ZSh
+cp ~/.zshrc $MY_FOLDER/Dotfiles/.zshrc
+cp -r ~/.zsh $MY_FOLDER/Dotfiles/.zsh
