e44d0b046f3354effd78c77c1d3ff939f671a3ca
contre95 <lucascontre95@gmail.com>
2020-09-17 12:45:05 -0300

    Remove acc

--- Scripts/dmenu-scr/set-display.sh
+++ Scripts/dmenu-scr/set-display.sh
@@ -2,7 +2,7 @@
 source ~/.zshrc
 SCR_PATH=$MY_FOLDER/Scripts
 
-choices="Single\nDual Up Inverted\nDual Up\nManual"
+choices="Single\nDual Up Inverted\nDual Up\nManual\nVertical"
 
 chosen=$(echo -e "$choices" | dmenu -i -p "Select Layout: " -sb "#7E33FF" )
 
@@ -10,5 +10,6 @@ case "$chosen" in
 	"Single") ${SCR_PATH}/xrandr-scr/singlelayout.sh ;;
 	"Dual Up Inverted") ${SCR_PATH}/xrandr-scr/cuartolayout.sh ;;
 	"Dual Up") ${SCR_PATH}/xrandr-scr/dualup.sh ;;
+	"Vertical") ${SCR_PATH}/xrandr-scr/vertical.sh ;;
 	"Manual") /usr/bin/arandr;;
 esac
