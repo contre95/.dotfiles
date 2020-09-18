8dcddb639a681a5c4dccfb77eec473c05e8f9623
contre95 <lucascontre95@gmail.com>
2020-09-17 22:23:39 -0300

    Remove acc

--- Scripts/dmenu-scr/set-display.sh
+++ Scripts/dmenu-scr/set-display.sh
@@ -2,14 +2,13 @@
 source ~/.zshrc
 SCR_PATH=$MY_FOLDER/Scripts
 
-choices="Single\nDual Up Inverted\nDual Up\nManual\nVertical"
+choices="Single\nDual\nManual\nVertical"
 
 chosen=$(echo -e "$choices" | dmenu -i -p "Select Layout: " -sb "#7E33FF" )
 
 case "$chosen" in
-	"Single") ${SCR_PATH}/xrandr-scr/singlelayout.sh ;;
-	"Dual Up Inverted") ${SCR_PATH}/xrandr-scr/cuartolayout.sh ;;
-	"Dual Up") ${SCR_PATH}/xrandr-scr/dualup.sh ;;
-	"Vertical") ${SCR_PATH}/xrandr-scr/vertical.sh ;;
+	"Single")  xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 2560x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off ;;
+	"Dual") xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 2560x1080 --pos 0x483 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --mode 1920x1080 --pos 2560x483 --rotate normal ;;
+	"Vertical") xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 2560x1080 --pos 0x483 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --mode 1920x1080 --pos 2560x0 --rotate right ;;
 	"Manual") /usr/bin/arandr;;
 esac
