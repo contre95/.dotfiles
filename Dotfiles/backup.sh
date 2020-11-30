db3f3788a08502a4f9168cce35e23dcc438305d6
contre95 <lucas.contreras@mercadolibre.com>
2020-11-30 11:14:36 -0300

    asd

--- Dotfiles/backup.sh
+++ Dotfiles/backup.sh
@@ -14,13 +14,14 @@ cp ~/.tmux.conf $MY_FOLDER/Dotfiles/.tmux.conf
 cp ~/.config/gtk-3.0/settings.ini $MY_FOLDER/Dotfiles/.config/gtk-3.0/settings.ini
 
 # Ranger
-cp -r ~/.config/ranger $MY_FOLDER/Dotfiles/.config/ranger
+cp -r ~/.config/ranger $MY_FOLDER/Dotfiles/.config
 
 # Vim 
-cp -r ~/.vim/colors $MY_FOLDER/Dotfiles/.vim/colors
-cp -r ~/.vim/syntax $MY_FOLDER/Dotfiles/.vim/syntax
+cp -r ~/.vim/colors $MY_FOLDER/Dotfiles/.vim
+cp -r ~/.vim/syntax $MY_FOLDER/Dotfiles/.vim
+cp -r ~/.vim/spell $MY_FOLDER/Dotfiles/.vim
 cp ~/.vimrc $MY_FOLDER/Dotfiles/.vimrc
 
 # ZSh
 cp ~/.zshrc $MY_FOLDER/Dotfiles/.zshrc
-cp -r ~/.zsh $MY_FOLDER/Dotfiles/.zsh
+cp -r ~/.zsh $MY_FOLDER/Dotfiles
