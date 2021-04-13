4b5ce00ae720940d90e33ead59327334f15b81c8
contre95 <lucascontre95@gmail.com>
2021-04-13 20:44:44 -0300

    :music:

--- Scripts/dmenu-scr/services.sh
+++ Scripts/dmenu-scr/services.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-choices=" OpenSSH\n Docker\n Bluetooth\n Droidcam"
+choices=" OpenSSH\n Docker\n Bluetooth\n Droidcam\n Deemix"
 var=$(echo -e $choices | dmenu -i -p "Exit menu: " -sb "#1ba4e9" -fn "JetBrainsMono Nerd Font") 
 
 case $var in
@@ -13,5 +13,18 @@ case $var in
     systemctl start docker;;
 
     ' Droidcam')
-    /usr/bin/droidcam-cli -v 192.168.0.192 4747 &
+    /usr/bin/droidcam-cli -v 192.168.0.192 4747 &;; 
+    
+    ' Deemix')
+    /usr/bin/docker run -d --name Deemix \
+              -v /home/canus/Music/:/downloads \
+              -v /home/contre/.config/deemix-server:/config \
+              -e PUID=1000 \
+              -e PGID=1000 \
+              -e ARL=1234567 \
+              -e UMASK_SET=022 \
+              -e DEEZUI=false \
+              -p 6595:6595 \
+              registry.gitlab.com/bockiii/deemix-docker;;
+
 esac
