1f33f72f66e1766c573d8c8c7e263a68929248a1
contre95 <lucascontre95@gmail.com>
2021-05-18 16:00:57 -0300

    Updated CV

--- Scripts/tmux-scr/mac.sh
+++ Scripts/tmux-scr/mac.sh
@@ -4,13 +4,13 @@ function macos{
     tmux start-server
 
     tmux new-session -d -s "  Mac " -n "  Mac " &&
-	tmux send-keys -t "  Mac ":1 'sshuttle -r lcontreras@localhost:2210 0.0.0.0/0 -vvv' C-m &&
+    tmux send-keys -t "  Mac ":1 'ssh -D 9890 lcontreras@localhost -p 2210' C-m &&
+	#tmux send-keys -t "  Mac ":1 'sshuttle -r lcontreras@localhost:2210 0.0.0.0/0 -vvv' C-m &&
 
-    tmux split-window -h -t "  Mac ":1
-    tmux send-keys -t "  Mac ":1 'ssh lcontreras@localhost -p 2210' C-m &&
-    resize-pane -L 110 &&
+    #tmux split-window -h -t "  Mac ":1
+    #resize-pane -L 110 &&
     
-    tmux select-window -t "  Mac ":1
+    tmux select-window -t "  Mac "
     tmux -u attach-session -t "  Mac "
 }
 
