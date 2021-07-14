a55b9427204883332849f40fc8d22bb601dc9c82
contre95 <lucascontre95@gmail.com>
2021-07-14 12:14:05 -0300

    Termux

---
+++ Scripts/tmux-scr/termux.sh
@@ -0,0 +1,11 @@
+
+function contreTermux{
+    tmux start-server
+    tmux new-session -d -s Android -n Server
+	
+	tmux send-keys -t Android:1 "ssh -p 8022 u0_a235@192.168.0.192" C-m
+
+    tmux attach-session -t Android
+}
+
+contreTermux
