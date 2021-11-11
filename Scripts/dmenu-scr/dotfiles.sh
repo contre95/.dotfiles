e185359521b1c240760a24f862e06534bb0301a4
contre95 <lucascontre95@gmail.com>
2021-11-11 23:00:35 +0100

    set autochdir :heart:

--- Scripts/dmenu-scr/dotfiles.sh
+++ Scripts/dmenu-scr/dotfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-choices="sxhkd\nbspwm\nvimrc"
+choices="sxhkd\nbspwm\nvimrc\nlemonbar"
 var=$(echo -e $choices | dmenu -i -p "Audio Output" -sb "#40444B" -fn "JetBrainsMono Nerd Font") 
 TMUX_SCRIPTS=/home/canus/Scripts/tmux-scr
 
@@ -10,8 +10,11 @@ case $var in
     choice="$HOME/.config/bspwm/bspwmrc";;
     'sxhkd')
     choice="$HOME/.config/sxhkd/sxhkdrc";;
+    'lemonbar')
+    choice="$HOME/.config/lemonbar/bar.py";;
 esac
 
 #if [[ ! "$choice" -eq "" ]];then
-    alacritty -e nvim "$choice"
+    alacritty -e nvim "$choice" -c "set autochdir"
+
 #fi
