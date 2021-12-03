9876eef628d5ab2ce547eee662dc3012141c7eab
Lucas Contreras <lucascontre95@gmail.com>
2021-12-03 20:16:06 -0300

    vsnips

--- Dotfiles/.config/nvim/lua/language-server.lua
+++ Dotfiles/.config/nvim/lua/language-server.lua
@@ -1,5 +1,18 @@
 require'lspconfig'.pyright.setup{}
 require'lspconfig'.gopls.setup{}
 require'lspconfig'.terraformls.setup{} 
-require'lspconfig'.tsserver.setup{}
 require'lspconfig'.sqls.setup{}
+require'lspconfig'.tsserver.setup{}
+
+-- go install github.com/mattn/efm-langserver@latest
+require'lspconfig'.efm.setup {
+    init_options = {documentFormatting = true},
+    settings = {
+        rootMarkers = {".git/"},
+        languages = {
+            lua = {
+                {formatCommand = "lua-format -i", formatStdin = true}
+            }
+        }
+    }
+}
