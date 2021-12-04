37ce98428ba2352191c748873a2fd2c999724af1
contre95 <lucascontre95@gmail.com>
2021-12-04 01:59:25 +0100

    Remove EFM

--- Dotfiles/.config/nvim/lua/language-server.lua
+++ Dotfiles/.config/nvim/lua/language-server.lua
@@ -3,16 +3,3 @@ require'lspconfig'.gopls.setup{}
 require'lspconfig'.terraformls.setup{} 
 require'lspconfig'.sqls.setup{}
 require'lspconfig'.tsserver.setup{}
-
--- go install github.com/mattn/efm-langserver@latest
-require'lspconfig'.efm.setup {
-    init_options = {documentFormatting = true},
-    settings = {
-        rootMarkers = {".git/"},
-        languages = {
-            lua = {
-                {formatCommand = "lua-format -i", formatStdin = true}
-            }
-        }
-    }
-}
