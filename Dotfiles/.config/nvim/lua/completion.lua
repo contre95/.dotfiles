e1ca3a9102aa7174a8cd04dfab7c0d4a25d97cd3
Lucas Contreras <lucascontre95@gmail.com>
2021-12-03 23:30:13 -0300

    vsnips

--- Dotfiles/.config/nvim/lua/completion.lua
+++ Dotfiles/.config/nvim/lua/completion.lua
@@ -1,27 +1,61 @@
-  -- Setup nvim-cmp.
-  local cmp = require'cmp'
-  local lspkind = require('lspkind')
-  local luasnip = require('luasnip')
 
-  cmp.setup({
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
+local cmp = require'cmp'
+local lspkind = require('lspkind')
+cmp.setup({
 
     mapping = {
-  ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-  ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' })
+      ['<CR>'] = cmp.mapping.confirm({
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = true,
+      }),
+
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            cmp.select_next_item()
+          elseif vim.fn["vsnip#available"](1) == 1 then
+            feedkey("<Plug>(vsnip-expand-or-jump)", "")
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+          end
+      end, { "i", "s" }),
+
+      ["<S-Tab>"] = cmp.mapping(function()
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+              feedkey("<Plug>(vsnip-jump-prev)", "")
+            end
+      end, { "i", "s" }),
     },
 
     sources =  cmp.config.sources({
       { name = 'nvim_lsp' },
-      { name = 'luasnip' },
+      { name = 'vsnip' },
       { name = 'path' },
       { name = 'buffer', keyword_length = 3, }
     }),
 
     snippet = {
       expand = function(args)
-        require("luasnip").lsp_expand(args.body)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
       end,
     },
+    --snippet = {
+      --expand = function(args)
+        --require("vsnip").lsp_expand(args.body)
+      --end,
+    --},
 
    formatting = {         
     --format = lspkind.cmp_format({with_text = false, maxwidth = 50})
@@ -31,7 +65,7 @@
         buffer = "[buf]",
         nvim_lsp = "[LSP]",
         path = "[path]",
-        luasnip = "[snip]",
+        vsnip = "[snip]",
         gh_issues = "[issues]",
         tn = "[TabNine]",
       },
