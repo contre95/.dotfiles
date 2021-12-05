6eb9ef082d43d13434270fe6021c23bec2457663
contre95 <lucascontre95@gmail.com>
2021-12-05 01:03:45 +0100

    LSP

--- Dotfiles/.config/nvim/vim-commands.vim
+++ Dotfiles/.config/nvim/vim-commands.vim
@@ -1,5 +1,7 @@
 "command! -nargs=0 Prettier :CocCommand prettier.formatFile
+command! -nargs=0 Format lua vim.lsp.buf.formatting()
 command! -nargs=0 Trans call LetMeSee()
+
 
 function LetMeSee()
     highlight NonText guibg=NONE
--- Scripts/dmenu-scr/otpmenu.sh
+++ Scripts/dmenu-scr/otpmenu.sh
@@ -9,18 +9,20 @@ if [[ $1 == "--type" ]]; then
 fi
 
 prefix=${PASSWORD_STORE_DIR-~/.password-store}
-password_files=( "$prefix"/**/OTP/*.gpg )
-password_files=( "${password_files[@]#"$prefix"/}" )
-password_files=( "${password_files[@]%.gpg}" )
+password_files=("$prefix"/**/OTP/*.gpg)
+password_files=("${password_files[@]#"$prefix"/}")
+password_files=("${password_files[@]%.gpg}")
 
-password=$(printf '%s\n' "${password_files[@]}" | dmenu -i -p "OTP :: " "$@" -sb "#34495A" -fn "JetBrainsMono Nerd Font" )
+password=$(printf '%s\n' "${password_files[@]}" | dmenu -i -p "OTP :: " "$@" -sb "#34495A" -fn "JetBrainsMono Nerd Font")
 
 [[ -n $password ]] || exit
 
 if [[ $typeit -eq 0 ]]; then
 	pass otp -c "$password" 2>/dev/null
 else
-	pass otp "$password" | { IFS= read -r pass; printf %s "$pass"; } |
+	pass otp "$password" | {
+		IFS= read -r pass
+		printf %s "$pass"
+	} |
 		xdotool type --clearmodifiers --file -
 fi
-
