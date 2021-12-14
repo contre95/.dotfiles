ba373e4d4b9747c5bffb3f50ed2bf6e57d3b077f
Lucas Contreras <lucas.contreras@n26.com>
2021-12-14 11:24:56 +0100

    tflint

--- Dotfiles/.config/nvim/lua/lsp.lua
+++ Dotfiles/.config/nvim/lua/lsp.lua
@@ -72,7 +72,6 @@ lspconfig.terraformls.setup({
 	capabilities = capabilities,
 	on_attach = function(client)
 		client.resolved_capabilities.document_formatting = true
-		on_attach(client)
 	end,
 	cmd = { "terraform-ls", "serve" },
 	filetypes = { "tf", "terraform" },
