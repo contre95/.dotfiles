71cb7de32a4d7ee6c1f90de910afd44c2379342e
contre95 <lucascontre95@gmail.com>
2021-12-18 20:56:21 +0100

    Remarkable ssh (re)stream

--- Scripts/dmenu-scr/services.sh
+++ Scripts/dmenu-scr/services.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
-choices=" OpenSSH\n Docker\n Bluetooth\n Droidcam\n Deemix"
+choices=" OpenSSH\n Docker\n Bluetooth\n Droidcam\n Deemix\n Remarkable"
 var=$(echo -e $choices | dmenu -i -p "Exit menu: " -sb "#1ba4e9" -fn "JetBrainsMono Nerd Font") 
 
 case $var in
     ' OpenSSH') 
-    /usr/bin/systemct start sshd ;;
+    systemctl start sshd ;;
    
     ' Bluetooth')
     systemctl start bluetooth ;;
@@ -14,13 +14,17 @@ case $var in
 
     ' Droidcam')
     /usr/bin/droidcam-cli -v 192.168.1.192 4747 &;; 
+
+    ' Remarkable')
+     eval `ssh-agent -s` ;
+    /usr/bin/ssh-add ~/.ssh/remarkable  ;
+    ~/Applications/reStream/reStream.sh 2> ~/templog;;
     
     ' Deemix')
         /usr/bin/podman run -d --cap-add CAP_NET_RAW \
               --rm \
-              --name DeTest \
               -v /home/canus/Music/:/deemix-gui/server/music \
               -v /home/contre/.config/deemix-server:/config \
               -p 6595:6595 \
-              -it registry.gitlab.com/bockiii/deemix-docker bash
+              registry.gitlab.com/bockiii/deemix-docker
 esac
