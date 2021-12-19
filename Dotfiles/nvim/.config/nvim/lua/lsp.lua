e46937c8faf7b7660c43f28228139e04da9e6068
contre95 <lucascontre95@gmail.com>
2021-12-19 22:44:38 +0100

    Rust tool

--- Dotfiles/nvim/.config/nvim/lua/lsp.lua
+++ Dotfiles/nvim/.config/nvim/lua/lsp.lua
@@ -1,140 +1,135 @@
 local lspconfig = require("lspconfig")
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp                                                         .protocol
+                                                                     .make_client_capabilities())
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    virtual_text = false,
+    signs = true,
+    update_in_insert = true,
+  }
+)
+
 
 -- Linters and Prettiers
 -----------------------------
 
 -- ES Linter -- npm i -g eslint_d
 local eslint = {
-	lintCommand = "eslint_d -f visualstudio --stdin --stdin-filename ${INPUT}",
-	lintIgnoreExitCode = true,
-	lintStdin = true,
-	lintFormats = { "%f(%l,%c): %tarning %m", "%f(%l,%c): %rror %m" },
-	lintSource = "eslint",
+    lintCommand = "eslint_d -f visualstudio --stdin --stdin-filename ${INPUT}",
+    lintIgnoreExitCode = true,
+    lintStdin = true,
+    lintFormats = {"%f(%l,%c): %tarning %m", "%f(%l,%c): %rror %m"},
+    lintSource = "eslint"
 }
 
 -- General prettier -- npm i -g prettier
 local prettier = {
-	formatCommand = "prettier --stdin-filepath ${INPUT}",
-	formatStdin = true,
+    formatCommand = "prettier --stdin-filepath ${INPUT}",
+    formatStdin = true
 }
 
 -- Lua linter -- cargo install stylua
 local luafmt = {
-	formatCommand = "stylua -s --stdin-filepath ${INPUT} -",
-	formatStdin = true,
+    formatCommand = "stylua -s --stdin-filepath ${INPUT} -",
+    formatStdin = true
 }
 
 -- Shell Formatter / Checker
 local shell = {
-	formatCommand = "shfmt ${-i:tabWidth}",
-	lintCommand = "shellcheck -f gcc -x -",
-	lintStdin = true,
-	lintFormats = { "%f:%l:%c: %trror: %m", "%f:%l:%c: %tarning: %m", "%f:%l:%c: %tote: %m" },
+    formatCommand = "shfmt ${-i:tabWidth}",
+    lintCommand = "shellcheck -f gcc -x -",
+    lintStdin = true,
+    lintFormats = {
+        "%f:%l:%c: %trror: %m", "%f:%l:%c: %tarning: %m", "%f:%l:%c: %tote: %m"
+    }
 }
 
--- LSP Configuration
+-- Languages Configuration
 -----------------------------
 
 -- Json
-lspconfig.jsonls.setup({ capabilities = capabilities })
+lspconfig.jsonls.setup({capabilities = capabilities})
 
 -- Rust
+require'lsp_extensions'.inlay_hints()
 lspconfig.rust_analyzer.setup({
-	capabilities = capabilities,
-
-	settings = {
-		["rust-analyzer"] = {
-			cargo = {
-				loadOutDirsFromCheck = true,
-			},
-			procMacro = {
-				enable = true,
-			},
-		},
-	},
+    capabilities=capabilities,
+    settings = {
+      ["rust-analyzer"] = {
+        checkOnSave = {
+          command = "clippy"
+        },
+      }
+    }
 })
 
 -- Python
-lspconfig.pyright.setup({ capabilities = capabilities })
+lspconfig.pyright.setup({capabilities = capabilities})
 
 -- Golang
-lspconfig.gopls.setup({ capabilities = capabilities })
+lspconfig.gopls.setup({capabilities = capabilities})
 
 -- Terraform
 lspconfig.tflint.setup({})
 lspconfig.terraformls.setup({
-	capabilities = capabilities,
-	on_attach = function(client)
-		client.resolved_capabilities.document_formatting = true
-	end,
-	cmd = { "terraform-ls", "serve" },
-	filetypes = { "tf", "terraform" },
+    capabilities = capabilities,
+    on_attach = function(client)
+        client.resolved_capabilities.document_formatting = true
+    end,
+    cmd = {"terraform-ls", "serve"},
+    filetypes = {"tf", "terraform"}
 })
 
 -- SQL  -- go install github.com/lighttiger2505/sqls@latest
 lspconfig.sqls.setup({
-	--cmd = { "/path/to/sqls", "-config", "$HOME/.config/sqls/config.yml" },
-	capabilities = capabilities,
-	on_attach = function(client)
-		client.resolved_capabilities.execute_command = true
-		-- client.commands = require("sqls").commands -- Neovim 0.6+ only
+    -- cmd = { "/path/to/sqls", "-config", "$HOME/.config/sqls/config.yml" },
+    capabilities = capabilities,
+    on_attach = function(client)
+        client.resolved_capabilities.execute_command = true
+        -- client.commands = require("sqls").commands -- Neovim 0.6+ only
 
-		require("sqls").setup({})
-	end,
-	picker = "telescope",
+        require("sqls").setup({})
+    end,
+    picker = "telescope"
 })
 
 -- Typescript
-lspconfig.tsserver.setup({ capabilities = capabilities })
+lspconfig.tsserver.setup({capabilities = capabilities})
 
 -- General purpose LS
 -- Languages setup
 local languages = {
-	typescript = { prettier, eslint },
-	javascript = { prettier, eslint },
-	yaml = { prettier },
-	lua = { luafmt },
-	html = { prettier },
-	scss = { prettier },
-	css = { prettier },
-	markdown = { prettier },
-	sh = { shell },
-	zsh = { shell },
-	--json = { prettier },
-	--terraform = { terraform },
-	--rust = { rustfmt },
-	-- python = {autopep}
+    typescript = {prettier, eslint},
+    javascript = {prettier, eslint},
+    yaml = {prettier},
+    lua = {luafmt},
+    html = {prettier},
+    scss = {prettier},
+    css = {prettier},
+    markdown = {prettier},
+    sh = {shell},
+    zsh = {shell}
 }
 
 lspconfig.efm.setup({
-	root_dir = lspconfig.util.root_pattern(".git", "/home/canus/Scripts", "/home/contre"),
-	filetypes = vim.tbl_keys(languages),
-	cmd = {
-		"/home/contre/go/bin/efm-langserver",
-		"-logfile",
-		"/home/contre/.cache/nvim/lsp.log",
-		"-loglevel",
-		"5",
-	},
-	init_options = { documentFormatting = true, codeAction = true },
-	filetypes = {
-		"css",
-		"html",
-		"javascript",
-		"bash",
-		"sh",
-		"yaml",
-		"zsh",
-		"lua",
-		"markdown",
-		"python",
-	},
-	settings = {
-		languages = languages,
-		log_level = 1,
-		log_file = "/home/contre/.cache/nvim/lsp.log",
-	},
+    root_dir = lspconfig.util.root_pattern(".git", "/home/canus/Scripts",
+                                           "/home/contre"),
+    filetypes = vim.tbl_keys(languages),
+    cmd = {
+        "/home/contre/go/bin/efm-langserver", "-logfile",
+        "/home/contre/.cache/nvim/lsp.log", "-loglevel", "5"
+    },
+    init_options = {documentFormatting = true, codeAction = true},
+    filetypes = {
+        "css", "html", "javascript", "bash", "sh", "yaml", "zsh", "lua",
+        "markdown", "python"
+    },
+    settings = {
+        languages = languages,
+        log_level = 1,
+        log_file = "/home/contre/.cache/nvim/lsp.log"
+    }
 
-	-- on_attach = on_attach
+    -- on_attach = on_attach
 })
