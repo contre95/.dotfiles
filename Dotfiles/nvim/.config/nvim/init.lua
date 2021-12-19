212a0687aa92bf1074dcbf92d8eeb82718f063c3
contre95 <lucascontre95@gmail.com>
2021-12-19 23:51:38 +0100

    Diagnostics

--- Dotfiles/nvim/.config/nvim/init.lua
+++ Dotfiles/nvim/.config/nvim/init.lua
@@ -12,25 +12,23 @@
      \__\/           ~~~~                   \__\/    
 --]]
 
-
-
 -- Plugins
 require("plugins") -- Plugins installed with packer
 require("lsp") -- lspconfig
 require("finder") -- telescope
 require("snippets") -- vsnip
-require("completion")  -- nvim-cmp
-require('git-config') -- gitSigns
+require("completion") -- nvim-cmp
+require("git-config") -- gitSigns
 require("statusline") -- lualine
-require("nvim-tree-config") 
-require('blankline-config')
-require('md-fenced-config')
+require("nvim-tree-config")
+require("blankline-config")
+require("md-fenced-config")
 require("treesitter-config")
 require("quick-scope-config")
 require("bufferline-config")
 require("schlepp-config")
 require("colorscheme-config")
-require("rust-tools")
+
 -- Leader key
 vim.g.mapleader = " "
 
@@ -39,8 +37,8 @@ vim.cmd("colorscheme material")
 vim.g.material_style = "deep ocean"
 
 ---- Vim Imports
-vim.cmd("so ~/.config/nvim/misc.vim")
-vim.cmd("so ~/.config/nvim/mappings.vim")
 vim.cmd("so ~/.config/nvim/settings.vim")
+vim.cmd("so ~/.config/nvim/mappings.vim")
 vim.cmd("so ~/.config/nvim/commands.vim")
 vim.cmd("so ~/.config/nvim/filetypes.vim")
+vim.cmd("so ~/.config/nvim/misc.vim")
--- Dotfiles/nvim/.config/nvim/lua/plugins.lua
+++ Dotfiles/nvim/.config/nvim/lua/plugins.lua
@@ -1,55 +1,57 @@
--- Install Packer 
-local install_path = vim.fn.stdpath("data") ..
-                         "/site/pack/packer/start/packer.nvim"
+-- Install Packer
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    vim.fn.system({
-        "git", "clone", "--depth", "1",
-        "https://github.com/wbthomason/packer.nvim", install_path
-    })
-    vim.api.nvim_command("packadd packer.nvim")
+	vim.fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	vim.api.nvim_command("packadd packer.nvim")
 end
 
--- Use Packer 
+-- Use Packer
 local packer = require("packer")
 local use = packer.use
 
 return require("packer").startup(function()
-    use("marko-cerovac/material.nvim")
-    use("wbthomason/packer.nvim")
-    use("nvim-treesitter/nvim-treesitter")
-    use({
-        "nvim-lualine/lualine.nvim",
-        requires = {"kyazdani42/nvim-web-devicons", opt = true}
-    })
-    use({"kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons"})
-    use "lukas-reineke/indent-blankline.nvim"
-    use({"lewis6991/gitsigns.nvim", requires = {"nvim-lua/plenary.nvim"}})
-    use("akinsho/nvim-bufferline.lua")
-    use("fatih/vim-go")
-    use("tpope/vim-markdown")
-
-    use("hashivim/vim-terraform")
-    use("neovim/nvim-lspconfig")
-    use("hrsh7th/nvim-cmp")
-    use("hrsh7th/cmp-buffer")
-    use("hrsh7th/cmp-path")
-    use("nvim-lua/lsp_extensions.nvim")
-    use("hrsh7th/cmp-cmdline")
-    use("hrsh7th/cmp-vsnip")
-    use("hrsh7th/vim-vsnip-integ")
-    use("hrsh7th/vim-vsnip")
-    use("rafamadriz/friendly-snippets")
-    use("hrsh7th/cmp-nvim-lsp")
-    use("nvim-lua/lsp-status.nvim")
-    use("onsails/lspkind-nvim")
-    use("ekickx/clipboard-image.nvim")
-    use("unblevable/quick-scope")
-    use("zirrostig/vim-schlepp")
-    use("scrooloose/nerdcommenter")
-    use({
-        "nvim-telescope/telescope.nvim",
-        requires = {{"nvim-lua/plenary.nvim"}}
-    })
-    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make'}
+	use("marko-cerovac/material.nvim")
+	use("wbthomason/packer.nvim")
+	use("nvim-treesitter/nvim-treesitter")
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
+	use({ "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" })
+	use("lukas-reineke/indent-blankline.nvim")
+	use({ "lewis6991/gitsigns.nvim", requires = { "nvim-lua/plenary.nvim" } })
+	use("akinsho/nvim-bufferline.lua")
+	--use("fatih/vim-go")
+	use("tpope/vim-markdown")
+	use("hashivim/vim-terraform")
+	use("neovim/nvim-lspconfig")
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("nvim-lua/lsp_extensions.nvim")
+	use("hrsh7th/cmp-cmdline")
+	use("hrsh7th/cmp-vsnip")
+	use("hrsh7th/vim-vsnip-integ")
+	use("hrsh7th/vim-vsnip")
+	use("rafamadriz/friendly-snippets")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("nvim-lua/lsp-status.nvim")
+	use("onsails/lspkind-nvim")
+	use("ekickx/clipboard-image.nvim")
+	use("unblevable/quick-scope")
+	use("zirrostig/vim-schlepp")
+	use("scrooloose/nerdcommenter")
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
 end)
