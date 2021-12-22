9cf889e3b6548c2da599311736a5a6d145825a9b
contre95 <lucascontre95@gmail.com>
2021-12-22 22:15:26 +0100

    Update 22-12-21

--- Scripts/backup-scr/b4sd.sh
+++ Scripts/backup-scr/b4sd.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 # Update newsboatd
-cd ~/.newsboat || exit
-git add .
-git commit -m "Update $(date +"%d-%m-%y")"
-git push
+git_bkp() {
+	pushd "$1" || exit 1
+	git add . &&
+		if git commit -m "Update $(date +"%d-%m-%y")"; then
+			git push &&
+				notify-send "$1 updated" ":)" -i "$MY_FOLDER"/Library/icons/git.png
+		else
+			notify-send "Nothing to backup" "$1" -i "$MY_FOLDER"/Library/icons/git.png
+			exit 0
+		fi
+	exit 1
+}
+
+#if ! git_bkp ~/.newsboat/; then
+	#notify-send "Error" "Could not back up newsboat :(" -i "$MY_FOLDER"/Library/icons/error.png
+#fi
+if ! git_bkp /home/canus; then
+	notify-send "Error" "Could not back up newsboat :(" -i "$MY_FOLDER"/Library/icons/error.png
+fi
