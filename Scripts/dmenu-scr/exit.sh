75012687ae45e0d105387ae1a87559f25bcb9e2a
contre95 <lucascontre95@gmail.com>
2021-12-23 18:07:09 +0100

    Update 23-12-21

--- Scripts/dmenu-scr/exit.sh
+++ Scripts/dmenu-scr/exit.sh
@@ -5,7 +5,7 @@ var=$(echo -e "$choices" | dmenu -i -p "Exit menu: " -sb "#FF3855" -fn "JetBrain
 
 case $var in
     ' Shutdown') 
-      /home/canus/Scripts/backup-scr/b4sd.sh && notify-send "Shtting down";;
+      /home/canus/Scripts/backup-scr/b4sd.sh && systemctl poweroff;;
 
     ' Lock')
       dm-tool lock ;;
