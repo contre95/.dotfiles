cb2cc106034999ad3153b70d1b0501171b5682f1
Lucas Contreras <lucascontre95@gmail.com>
2021-12-23 12:56:19 -0300

    Update 23-12-21

--- Scripts/backup-scr/b4sd.sh
+++ Scripts/backup-scr/b4sd.sh
@@ -3,17 +3,17 @@
 git_bkp() {
 	pushd "$1" || exit 1
 	git add . &&
-		if git commit -m "Update $(date +"%d-%m-%y")"; then
-			git push &&
-				notify-send "$1 updated" ":)" -i "$MY_FOLDER"/Library/icons/git.png
-		else
-			notify-send "Nothing to backup" "$1" -i "$MY_FOLDER"/Library/icons/git.png
-			exit 0
-		fi
+	if git commit -m "Update $(date +"%d-%m-%y")"; then
+		git push &&
+			notify-send "$1 updated" ":)" -i "$MY_FOLDER"/Library/icons/git.png
+	else
+		notify-send "Nothing to backup" "$1" -i "$MY_FOLDER"/Library/icons/git.png
+		exit 0
+	fi
 	git push &&
 		popd
 }
 
-git_bkp /home/canus 2&> xargs -I {} "notify-send "Error" "{}" -i "$MY_FOLDER"/Library/icons/error.png &&
+git_bkp /home/canus | xargs -I {} "notify-send "Error" "{}" -i "$MY_FOLDER"/Library/icons/error.png &&
 git_bkp /.newsboat 2&> xargs -I {} "notify-send "Error" "{}" -i "$MY_FOLDER"/Library/icons/error.png
 #git_bkp ~/.newsboat/ && notify-send "Error" "Could not back up newsboat :(" -i "$MY_FOLDER"/Library/icons/error.png
