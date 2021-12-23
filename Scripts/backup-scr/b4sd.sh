18fc344536494615c499c79dce410cd2e292521b
contre95 <lucascontre95@gmail.com>
2021-12-23 17:38:15 +0100

    Update 23-12-21

--- Scripts/backup-scr/b4sd.sh
+++ Scripts/backup-scr/b4sd.sh
@@ -4,8 +4,7 @@ git_bkp() {
 	pushd "$1" || exit 1
 	notify-send "$1" "updating.." -i "$MY_FOLDER"/Library/icons/git.png
 	if git add . && git commit -m "Update $(date +'%d-%m-%y')"; then
-      sleep 1 &&
-		git push 2> ~/tmp.log > ~/tmp.log &&
+		git push &&
 			notify-send "$1 updated" ":)" -i "$MY_FOLDER"/Library/icons/success.png
 	else
 		notify-send "Nothing to backup" "$1" -i "$MY_FOLDER"/Library/icons/info.png
