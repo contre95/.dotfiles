b31f9e6c47ae0c1ebe928160669b60c1e1b31c2a
contre95 <lucascontre95@gmail.com>
2021-12-29 18:59:07 +0100

    loopback-module gracias Charles

---
+++ Scripts/audio-scr/mic-loopback.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Loads / unloads pulseaudio loopback module in order to eg. be redirect
+#  mic input to headphones.
+#
+# Usage: mic-loopback [on|off]
+#
+
+#LOOPBACKMODULES=( $(pactl list short modules | awk '$2 =="module-loopback" { print $1 }') )
+mapfile -t LOOPBACKMODULES < <(pactl list short modules | awk '$2 =="module-loopback" { print $1 }') # mapfile is only available in bash>=4
+
+if [[ ${#LOOPBACKMODULES[@]} -gt 0 ]]; then # 1 becaouse for some reason there's an empty something in the array (don't wanna bother)
+	pactl list short modules | awk '$2 =="module-loopback" { print $1 }' | xargs -I {} -t pactl unload-module {}
+else
+	pactl load-module module-loopback latency_msec=1
+fi
