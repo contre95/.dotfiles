249aabcccc909a095c367b9b845cf1a65e866980
Lucas Contreras <lucascontre95@gmail.com>
2021-12-30 10:17:36 -0300

    Update 30-12-21

--- Scripts/backup-scr/b4sd.sh
+++ Scripts/backup-scr/b4sd.sh
@@ -4,6 +4,7 @@ export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 
 git_bkp() {
 	pushd "$1" || exit 1
+    git pull
 	notify-send "$1" "updating.." -i "$MY_FOLDER"/Library/icons/git.png
 	if git add . && git commit -m "Update $(date +'%d-%m-%y')"; then
 		if git push 2> ~/err.log; then
@@ -17,6 +18,7 @@ git_bkp() {
 	popd || exit 1
 }
 
+pass git push
 git_bkp /home/canus
 git_bkp /home/contre/.newsboat
 #git_bkp /.newsboat 2&> xargs -I {} "notify-send "Error" "{}" -i "$MY_FOLDER"/Library/icons/error.png"
