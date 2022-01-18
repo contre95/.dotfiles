d3c03309117ce57d253b6a9ebf98966ae1bcc020
contre95 <lucascontre95@gmail.com>
2022-01-18 22:10:12 +0100

    Web light

--- Scripts/dmenu-scr/web-light.sh
+++ Scripts/dmenu-scr/web-light.sh
@@ -1,13 +1,21 @@
 #!/bin/zsh
 source ~/.zshrc
 SCR_PATH=$MY_FOLDER/Scripts
-choices="N26\nGalicia Contre\nMercadopago Contre\nGalicia Codelamp\nMercadopago Codelamp"
+choices="Any\nN26\nGalicia Contre\nMercadopago Contre\nGalicia Codelamp\nMercadopago Codelamp\nYoutube\nTwitter\nDeemix"
 
 chosen=$(echo -e "$choices" | dmenu -i -p "Select Layout: " -sb "#FF5733")
 
 case "$chosen" in
 	"N26")
         exec brave --profile-directory="Contre" --app=https://app.n26.com/login;;
+	"Deemix")
+        exec brave --profile-directory="Contre" --app=http://localhost:6595;;
+	"Youtube")
+        exec brave --profile-directory="Contre" --app=https://youtube.com;;
+	"Twitter")
+        exec brave --profile-directory="Contre" --app=https://twitter.com;;
+	"Any")
+      exec brave --profile-directory="Contre" --app=https://$(zenity --entry --text="Website  :");;
 	"Galicia Contre")
         exec brave --profile-directory="Contre" --app=https://onlinebanking.bancogalicia.com.ar/login;;
 	"Galicia Codelamp")
