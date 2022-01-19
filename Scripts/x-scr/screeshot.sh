1ebda3b1c9cfc9d13607bb54cea4cbf892df2322
contre95 <lucascontre95@gmail.com>
2022-01-19 17:29:50 +0100

    Update 19-01-22

---
+++ Scripts/x-scr/screeshot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SAVE_SCREENSHOT_PATH=~/Pictures/Screenshots/screen$(/bin/date +%Y%m%d-%H:%M:%S).png
+
+import "$SAVE_SCREENSHOT_PATH" &&
+  	notify-send "Screenshots saved" "$SAVE_SCREENSHOT_PATH" -i "$MY_FOLDER"/Library/icons/display.png
