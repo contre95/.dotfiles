791e855afc0c28d2cb115be3ef5694f52eab9e74
contre95 <lucascontre95@gmail.com>
2022-01-23 16:31:59 +0100

    Update 23-01-22

--- Scripts/dmenu-scr/services.sh
+++ Scripts/dmenu-scr/services.sh
@@ -1,30 +1,37 @@
 #!/bin/zsh
 choices=" OpenSSH\n Docker\n Bluetooth\n Droidcam\n Deemix\n Remarkable"
-var=$(echo -e $choices | dmenu -i -p "Exit menu: " -sb "#1ba4e9" -fn "JetBrainsMono Nerd Font") 
+var=$(echo -e $choices | dmenu -i -p "Exit menu: " -sb "#1ba4e9" -fn "JetBrainsMono Nerd Font")
 
 case $var in
-    ' OpenSSH') 
-    systemctl start sshd ;;
-   
-    ' Bluetooth')
-    systemctl start bluetooth ;;
-      
-    ' Docker')
-    systemctl start docker;;
+' OpenSSH')
+	systemctl start sshd
+	;;
 
-    ' Droidcam')
-    /usr/bin/droidcam-cli -v 192.168.1.192 4747 &;; 
+' Bluetooth')
+	systemctl start bluetooth
+	;;
 
-    ' Remarkable')
-     eval `ssh-agent -s` ;
-    /usr/bin/ssh-add ~/.ssh/remarkable  ;
-    ~/Applications/reStream/reStream.sh 2> ~/templog;;
-    
-    ' Deemix')
-        /usr/bin/podman run -d --cap-add CAP_NET_RAW \
-              --rm \
-              -v /home/canus/Music/:/deemix-gui/server/music \
-              -v /home/contre/.config/deemix-server:/config \
-              -p 6595:6595 \
-              registry.gitlab.com/bockiii/deemix-docker
+' Docker')
+	systemctl start docker
+	;;
+
+' Droidcam')
+	/usr/bin/droidcam-cli -v 192.168.1.192 4747 &
+	;;
+
+' Remarkable')
+	eval $(ssh-agent -s)
+	/usr/bin/ssh-add ~/.ssh/remarkable
+	~/Applications/reStream/reStream.sh 2>~/templog
+	;;
+
+' Deemix')
+	/usr/bin/podman run -d --cap-add CAP_NET_RAW \
+		--rm \
+		-v /home/canus/Music/:/deemix-gui/server/music \
+		-v /home/contre/.config/deemix-server:/config \
+		-p 6595:6595 \
+		registry.gitlab.com/bockiii/deemix-docker
+	;;
+
 esac
