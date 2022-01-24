fd37fb75776f38fa0480a21b00db94b2fa9d797f
contre95 <lucascontre95@gmail.com>
2022-01-24 17:17:36 +0100

    Update 24-01-22

--- Scripts/audio-scr/mic-mute.sh
+++ Scripts/audio-scr/mic-mute.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#mapfile -t LOOPBACKMODULES < <(pactl list short modules | awk '$2 =="module-loopback" { print $1 }') # mapfile is only available in bash>=4
 
-if [[ $(amixer -c 3 | grep "Playback \[off]") ]]; then # 1 becaouse for some reason there's an empty something in the array (don't wanna bother)
+if [[ $(echo always-true-fix) ]]; then # 1 becaouse for some reason there's an empty something in the array (don't wanna bother)
 	notify-send "Mic Monitor" "muted" -i "$MY_FOLDER"/Library/icons/muted.png
-	amixer -c 3 set Mic unmute
+    pactl set-source-mute alsa_input.usb-Samson_Technologies_Samson_C01U_Pro_Mic-00.analog-stereo toggle
 else
-	notify-send "Mic Monitor" "muted" -i "$MY_FOLDER"/Library/icons/mic.png
-	amixer -c 3 set Mic mute
+	notify-send "Mic Monitor" "unmuted" -i "$MY_FOLDER"/Library/icons/mic.png
+    pactl set-source-mute alsa_input.usb-Samson_Technologies_Samson_C01U_Pro_Mic-00.analog-stereo toggle
 fi
