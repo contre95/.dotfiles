36802858effd169dc84921b2a9ea26aa71de4091
contre95 <lucascontre95@gmail.com>
2022-09-03 17:30:47 +0200

    Update 03-09-22

--- Dotfiles/nvim/.config/nvim/lua/custom/run_aside.lua
+++ Dotfiles/nvim/.config/nvim/lua/custom/run_aside.lua
@@ -1,34 +1,37 @@
-local attach_to_buffer = function(bufnr, pattern, command)
-  vim.api.nvim_create_autocmd("BufWritePost", {
-    group = vim.api.nvim_create_augroup("TestingAugroup", { clear = true }),
-    pattern = pattern,
-    callback = function()
-      local append_data = function(_, data)
-        if data then
-          vim.api.nvim_buf_set_lines(bufnr, 0, 0,false, data)
-        end
-      end
-      vim.fn.jobstart(command, {
-        stdout_buffered = true,
-        on_stdout = append_data,
-        on_stderr = append_data,
-      })
-    end
-  })
-end
+--local attach_to_buffer = function(bufnr, pattern, command)
+  --vim.api.nvim_create_autocmd("BufWritePost", {
+    --group = vim.api.nvim_create_augroup("TestingAugroup", { clear = true }),
+    --pattern = pattern,
+    --callback = function()
+      --local jobstart_callback= function(_, data)
+        --if not data then
+          --print("No data received")
+          --return
+        --end
+        --for _, line in ipairs(data) do
+          --print(line)
+          --local decoded_line = vim.json.decode(line)
+          --if decoded_line.Action == 'run' then
+            ------vim.api.nvim_buf_set_lines(bufnr, 0, 0, false, decoded_line)
+          --elseif decoded_line.Action == 'output' then
+            ----vim.api.nvim_buf_set_lines(bufnr, 0, 0, false, decoded_line)
+          --end
+        --end
+      --end
+
+      --vim.fn.jobstart(command, {
+        --stdout_buffered = true,
+        --on_stdout = jobstart_callback, -- The vim.fn.jobstart() takes a callback func and passes the std<our/err> of of the "command"
+        --on_stderr = jobstart_callback,
+      --})
+    --end
+  --})
+--end
 
 
---attach_to_buffer(current_buf, "*.go", { "go", "run", "main.go" })
-
-vim.api.nvim_create_user_command('RunAsideOnSave', function()
-  local command = vim.fn.input("Command: ")
-  local pattern = vim.fn.input("Pattern: ")
-  vim.api.nvim_command('botright vsplit new') -- split a new window
-  local current_buf = vim.api.nvim_get_current_buf()
-  attach_to_buffer(current_buf, pattern, command)
-  vim.api.nvim_command('wincmd p') -- go back to previous window
-end, {})
+--local bufnr = vim.api.nvim_get_current_buf()
+--attach_to_buffer(bufnr, "*.go", { 'go', 'test', './...', '-json' })
+--#region
 
 
---jobID = vim.api.nvim_call_function("termopen", { "$SHELL" })
 
