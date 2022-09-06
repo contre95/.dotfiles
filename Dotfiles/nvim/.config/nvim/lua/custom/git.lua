1eee66ba557548e41f903a83559ef74566c60c7e
contre95 <lucascontre95@gmail.com>
2022-09-06 22:47:59 +0200

    Command del mas pijudo

--- Dotfiles/nvim/.config/nvim/lua/custom/git.lua
+++ Dotfiles/nvim/.config/nvim/lua/custom/git.lua
@@ -1,7 +1,16 @@
+-- Git Add/Commit
 vim.api.nvim_create_user_command('GitAdd', function ()
    local msg = vim.fn.input('Msg: ')
    local bufnr = vim.api.nvim_get_current_buf()
    local filename = vim.api.nvim_buf_get_name(bufnr)
    vim.fn.jobstart({"git", "add", filename})
    vim.fn.jobstart({"git", "commit", "-m", msg})
+end, {})
+
+vim.api.nvim_create_user_command('GitPush', function ()
+   vim.fn.jobstart({"git", "push"})
+end, {})
+
+vim.api.nvim_create_user_command('GitPull', function ()
+   vim.fn.jobstart({"git", "pull"})
 end, {})
