cd1e1e8218a8dbe44f76ec1b59decc7bebf744bf
contre95 <lucascontre95@gmail.com>
2022-09-06 23:25:59 +0200

    asd

--- Dotfiles/nvim/.config/nvim/lua/custom/git.lua
+++ Dotfiles/nvim/.config/nvim/lua/custom/git.lua
@@ -12,7 +12,6 @@ vim.api.nvim_create_user_command('GitPush', function ()
    vim.fn.jobstart({"git", "push"})
 end, {})
 
--- Git Pull
 vim.api.nvim_create_user_command('GitPull', function ()
    vim.fn.jobstart({"git", "pull"})
 end, {})
