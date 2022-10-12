cfdb5eee6f5f679714c7e9a4dad35aa67d1375b9
contre95 <lucascontre95@gmail.com>
2022-10-12 16:57:36 +0200

    Setting to lua

--- Dotfiles/nvim/.config/nvim/lua/commands.lua
+++ Dotfiles/nvim/.config/nvim/lua/commands.lua
@@ -1,18 +1,19 @@
--- Git Add/Commit
+-- git add/commit
 vim.api.nvim_create_user_command('GitAdd', function ()
-   local msg = vim.fn.input('Msg: ')
+   local msg = vim.fn.input('msg: ')
    local bufnr = vim.api.nvim_get_current_buf()
    local filename = vim.api.nvim_buf_get_name(bufnr)
    vim.fn.jobstart({"git", "add", filename})
    vim.fn.jobstart({"git", "commit", "-m", msg})
 end, {})
 
--- Git Push
+-- git push
 vim.api.nvim_create_user_command('GitPush', function ()
    vim.fn.jobstart({"git", "push"})
 end, {})
 
--- Git Pull
+-- git pull
 vim.api.nvim_create_user_command('GitPull', function ()
    vim.fn.jobstart({"git", "pull"})
 end, {})
+
