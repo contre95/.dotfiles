72e810e8b970d30318deb782a94e08a2ace56a19
contre95 <lucascontre95@gmail.com>
2022-12-04 05:59:42 +0100

    Nvim Debugger UI setup

--- Dotfiles/nvim/.config/nvim/lua/plugins/dap.lua
+++ Dotfiles/nvim/.config/nvim/lua/plugins/dap.lua
@@ -1,2 +1,20 @@
-require('dapui').setup({})
+require('dapui').setup({
+  icons = { expanded = "", collapsed = "ﬔ", current_frame = "" },
+  controls = {
+    -- Requires Neovim nightly (or 0.8 when released)
+    enabled = true,
+    -- Display controls in this element
+    element = "repl",
+    icons = {
+      pause = "",
+      play = "",
+      step_into = "",
+      step_over = "",
+      step_out = "",
+      step_back = "",
+      run_last = "ﰌ",
+      terminate = "",
+    },
+  },
+})
 require('dap-go').setup({})
