4064506cce28fd381f3633abd109f1c3c9681939
contre95 <lucascontre95@gmail.com>
2022-12-15 18:12:23 +0100

    Update 15-12-22

--- Scripts/tmux-scr/contre-server.sh
+++ Scripts/tmux-scr/contre-server.sh
@@ -4,7 +4,7 @@ function contreserver {
 	tmux start-server
 	tmux new-session -d -s Server -n SSH
 
-	tmux send-keys -t Server:1 "ssh contre@192.168.1.196" C-m
+	tmux send-keys -t Server:1 "ssh contre@192.168.0.174" C-m
 
 	tmux -u attach-session -t Server
 }
