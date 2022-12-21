da4e4a125cbb63e7d6248bcfaeff3a3138eeff4a
contre95 <lucascontre95@gmail.com>
2022-12-21 16:59:39 +0100

    196 174

--- Scripts/audio-scr/mic-mute.sh
+++ Scripts/audio-scr/mic-mute.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [[ $(echo always-true-fix) ]]; then # 1 becaouse for some reason there's an empty something in the array (don't wanna bother)
-	notify-send "Mic Monitor" "muted" -i "$MY_FOLDER"/Library/icons/muted.png
-    pactl set-source-mute alsa_input.usb-Samson_Technologies_Samson_C01U_Pro_Mic-00.analog-stereo toggle
-else
-	notify-send "Mic Monitor" "unmuted" -i "$MY_FOLDER"/Library/icons/mic.png
-    pactl set-source-mute alsa_input.usb-Samson_Technologies_Samson_C01U_Pro_Mic-00.analog-stereo toggle
-fi
+notify-send "Mic Monitor" "muted toggle" -i "$MY_FOLDER"/Library/icons/muted.png
+pactl set-source-mute 0 toggle # 0 is default
+
