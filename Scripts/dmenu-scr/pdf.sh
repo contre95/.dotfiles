a6b3b088a86818af44988eeb85ece407bebbda24
contre95 <lucascontre95@gmail.com>
2022-12-31 17:06:25 +0100

    Update 31-12-22

--- Scripts/dmenu-scr/pdf.sh
+++ Scripts/dmenu-scr/pdf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source ~/.zshrc 
 
-pdf=$(ls -1 $MY_FOLDER/PDFS | dmenu -i -p "Exit menu: " -sb "#0F111A" -fn "JetBrainsMono Nerd Font") 
+pdf=$(ls -1 "$MY_FOLDER"/PDFS | dmenu -i -p "Exit menu: " -sb "#0F111A" -fn "JetBrainsMono Nerd Font") 
 
 if [[ ! $pdf -eq "" ]];then
-    zathura $MY_FOLDER/PDFS/$pdf &
+    zathura "$MY_FOLDER/PDFS/$pdf" &
 fi
