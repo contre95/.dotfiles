2da8942d3581c19ccda8d5fca98651f6e447987d
contre95 <lucascontre95@gmail.com>
2022-12-31 17:06:25 +0100

    Update 31-12-22

--- Scripts/dmenu-scr/services.sh
+++ Scripts/dmenu-scr/services.sh
@@ -1,8 +1,9 @@
 #!/bin/zsh
-choices=" OpenSSH\n Docker\n Bluetooth\n Droidcam\n Deemix\n Remarkable"
+choices=" OpenSSH\n Bluetooth\n Droidcam\n Remarkable"
 var=$(echo -e $choices | dmenu -i -p "Exit menu: " -sb "#1ba4e9" -fn "JetBrainsMono Nerd Font")
 
 case $var in
+
 ' OpenSSH')
 	systemctl start sshd
 	;;
@@ -11,27 +12,14 @@ case $var in
 	systemctl start bluetooth
 	;;
 
-' Docker')
-	systemctl start docker
-	;;
-
 ' Droidcam')
 	/usr/bin/droidcam-cli -v 192.168.1.192 4747 &
 	;;
 
 ' Remarkable')
-	eval $(ssh-agent -s)
-	/usr/bin/ssh-add ~/.ssh/remarkable
+    eval $(ssh-agent -s)
+    /usr/bin/ssh-add ~/.ssh/remarkable
 	~/Applications/reStream/reStream.sh 2>~/templog
-	;;
-
-' Deemix')
-	/usr/bin/podman run -d --cap-add CAP_NET_RAW \
-		--rm \
-		-v /home/canus/Music/:/deemix-gui/server/music \
-		-v /home/contre/.config/deemix-server:/config \
-		-p 6595:6595 \
-		registry.gitlab.com/bockiii/deemix-docker
 	;;
 
 esac
