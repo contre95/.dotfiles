af3875d6b33b6784cd2b0786faadd947b3c5e43e
contre95 <lucascontre95@gmail.com>
2023-02-12 17:15:26 +0100

    :eyes:

--- Dotfiles/nvim/.config/nvim/lua/plugins/lsp.lua
+++ Dotfiles/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -97,7 +97,7 @@ lspconfig.jsonls.setup({ capabilities = capabilities })
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
-lspconfig.sumneko_lua.setup({
+lspconfig.lua_ls.setup({
   settings = {
     Lua = {
       runtime = {
