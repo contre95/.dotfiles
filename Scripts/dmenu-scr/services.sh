01c0c4b2e304f24aa454133bc3d85a0cab2265b2
contre95 <lucascontre95@gmail.com>
2023-02-16 10:50:40 +0100

    Barrier for mac os

--- Scripts/dmenu-scr/services.sh
+++ Scripts/dmenu-scr/services.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-choices=" OpenSSH\n Bluetooth\n Droidcam\n Remarkable"
+choices=" OpenSSH\n Bluetooth\n Droidcam\n Remarkable\n Barrier"
 var=$(echo -e $choices | dmenu -i -p "Exit menu: " -sb "#1ba4e9" -fn "JetBrainsMono Nerd Font")
 
 case $var in
@@ -14,6 +14,10 @@ case $var in
 
 ' Droidcam')
 	/usr/bin/droidcam-cli -v 192.168.1.192 4747 &
+	;;
+
+' Barrier')
+	barriers -f --no-tray  --disable-crypto --disable-client-cert-checking -c ~/.config/barrier/server/barrier.conf --address :24800 --restart &
 	;;
 
 ' Remarkable')
