7c4106d1670c257d66957104085a3a65b472bc9a
Lucas Contreras <lucascontre95@gmail.com>
2023-02-25 04:55:00 +0100

    I need to pass usecases into the graphql resolver

--- Scripts/dmenu-scr/web-light.sh
+++ Scripts/dmenu-scr/web-light.sh
@@ -6,17 +6,17 @@ chosen=$(echo -e "$choices" | dmenu -i -p "Select Layout: " -sb "#8F7DAB")
 
 # NOTE 
 # In order for librefox not to launch this windows in fullscreen (at lead on bspwm) we need to set up 'full-screen-api.ignore-widgets' to `true` in the about:config
-LW_PROFILE="/home/contre/.librewolf/bhadvuvg.ContreKiosk"
+LW_PROFILE="ContreKiosk"
 
 case "$chosen" in
 "N26")
-    librewolf --kiosk --profile $LW_PROFILE --new-window https://app.n26.com/login
+    librewolf --kiosk -P $LW_PROFILE --new-window https://app.n26.com/login
 	;;
 "TradingView")
-    librewolf --kiosk --profile $LW_PROFILE --new-window https://www.tradingview.com/chart/rH3IToRa/?symbol=BITSTAMP%3ABTCUSD
+    librewolf --kiosk -P $LW_PROFILE --new-window https://www.tradingview.com/chart/rH3IToRa/?symbol=BITSTAMP%3ABTCUSD
 	;;
 "White")
-    librewolf --kiosk --profile $LW_PROFILE --new-window https://blankwhitescreen.com/
+    librewolf --kiosk -P $LW_PROFILE --new-window https://blankwhitescreen.com/
 	;;
 "Keybr")
 	exec brave --profile-directory="Contre" --app=https://www.keybr.com/
@@ -25,45 +25,45 @@ case "$chosen" in
 	exec brave --profile-directory="Contre" --app=https://www.monkeytype.com/
 	;;
 "Twitch")
-    librewolf --kiosk --profile $LW_PROFILE --new-window https://twitch.tv/
+    librewolf --kiosk -P $LW_PROFILE --new-window https://twitch.tv/
 	;;
 "Cronometer")
-    librewolf --kiosk --profile $LW_PROFILE --new-window https://cronometer.com/
+    librewolf --kiosk -P $LW_PROFILE --new-window https://cronometer.com/
 	;;
 "Deemix")
-    librewolf --kiosk --profile $LW_PROFILE --new-window http://dmusic.contre.io
+    librewolf --kiosk -P $LW_PROFILE --new-window http://dmusic.contre.io
 	;;
 "ChatGPT")
-    librewolf --kiosk --profile $LW_PROFILE --new-window https://chat.openai.com/chat
+    librewolf --kiosk -P $LW_PROFILE --new-window https://chat.openai.com/chat
 	;;
 "Media")
-	librewolf --kiosk --profile $LW_PROFILE --new-window http://media.contre.io
+	librewolf --kiosk -P $LW_PROFILE --new-window http://media.contre.io
 	;;
 "Youtube")
     # In order for librefox not to launch this windows in fullscreen (at lead on bspwm) we need to set up full-screen-api.ignore-widgets to `true` in the about:config
-    librewolf --kiosk --profile $LW_PROFILE --new-window https://youtube.com
+    librewolf --kiosk -P $LW_PROFILE --new-window https://youtube.com
 	;;
 "Lichess")
-    librewolf --kiosk --profile $LW_PROFILE --new-window https://lichess.org
+    librewolf --kiosk -P $LW_PROFILE --new-window https://lichess.org
 	;;
 "Twitter")
-    librewolf --kiosk --profile $LW_PROFILE --new-window https://twitter.com
+    librewolf --kiosk -P $LW_PROFILE --new-window https://twitter.com
 	;;
 "Home")
-    # librewolf --kiosk --profile $LW_PROFILE --new-window http://home.contre.io/home-dash/0
+    # librewolf --kiosk -P $LW_PROFILE --new-window http://home.contre.io/home-dash/0
 	exec brave --profile-directory="Contre" --app=http://home.contre.io/home-dash/0
 	;;
 "Any")
-    librewolf --kiosk --profile $LW_PROFILE --new-window http://"$(zenity --entry --text='Website  :')"
+    librewolf --kiosk -P $LW_PROFILE --new-window http://"$(zenity --entry --text='Website  :')"
 	;;
 "New Bookmark")
    echo "$(zenity --entry --text='Bookmark:')" - http://"$(zenity --entry --text='Website  :')" >> $MY_FOLDER/Info/bookmarks.txt 
 	;;
 "Garmin")
-    librewolf --kiosk --profile $LW_PROFILE --new-window https://connect.garmin.com/modern/
+    librewolf --kiosk -P $LW_PROFILE --new-window https://connect.garmin.com/modern/
 	;;
 "Instagram")
-    librewolf --kiosk --profile $LW_PROFILE --new-window https://instagram.com
+    librewolf --kiosk -P $LW_PROFILE --new-window https://instagram.com
 	;;
 esac
 
