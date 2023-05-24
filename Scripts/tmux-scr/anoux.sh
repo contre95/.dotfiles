30efb141d38f16df91469412a2e452f036b505f1
contre95 <lucascontre95@gmail.com>
2023-05-24 17:35:47 +0200

    Update 24-05-23

--- Scripts/tmux-scr/anoux.sh
+++ Scripts/tmux-scr/anoux.sh
@@ -3,7 +3,7 @@ function contreAnoux {
     tmux start-server
     tmux new-session -d -s Anoux -n SSH
 	
-	tmux send-keys -t Anoux:1 "ssh anoux@192.168.1.180" C-m
+	tmux send-keys -t Anoux:1 "ssh anoux@192.168.0.191" C-m
 
     tmux attach-session -t Anoux
 }
