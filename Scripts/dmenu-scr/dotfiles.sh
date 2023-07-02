52bd6643701fae0d1ecc4fe74345caf3b3c4e692
Lucas Contreras <lucascontre95@gmail.com>
2023-07-02 15:58:23 +0200

    Option3

--- Scripts/dmenu-scr/dotfiles.sh
+++ Scripts/dmenu-scr/dotfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 choices="sxhkd\nbspwm\nvimrc\nlemonbar\nzsh\ntmux\nwaybar\nwaybar-css\nhyprland\nalacritty\nscripts"
-var=$(echo -e $choices | dmenu -m DP-3 -i -p "Dotfiles" -sb "#40444B" -fn "JetBrainsMono Nerd Font") 
+var=$(echo -e $choices | dmenu -i -p "Dotfiles" -sb "#40444B" -fn "JetBrainsMono Nerd Font") 
 TMUX_SCRIPTS=/home/canus/Scripts/tmux-scr
 
 case $var in
@@ -19,7 +19,7 @@ case $var in
     'lemonbar')
     choice="$HOME/.config/lemonbar/bar.py";;
     'scripts')
-    choice=$SCR_PATH/$(find $SCR_PATH -type f | sed "s|$SCR_PATH/||" | dmenu -m DP-3 -i -p "Scripts" -sb "#40444B" -fn "JetBrainsMono Nerd Font");;
+    choice=$SCR_PATH/$(find $SCR_PATH -type f | sed "s|$SCR_PATH/||" | dmenu -i -p "Scripts" -sb "#40444B" -fn "JetBrainsMono Nerd Font");;
     'alacritty')
     choice="$HOME/.config/alacritty/alacritty.yml";;
     'zsh')
--- Scripts/dmenu-scr/passmenu.sh
+++ Scripts/dmenu-scr/passmenu.sh
@@ -13,7 +13,7 @@ password_files=( "$prefix"/**/*.gpg )
 password_files=( "${password_files[@]#"$prefix"/}" )
 password_files=( "${password_files[@]%.gpg}" )
 
-password=$(printf '%s\n' "${password_files[@]}" | dmenu -m DP-3 -i -p "Pass  : " "$@" -sb "#34495E" -fn "JetBrainsMono Nerd Font")
+password=$(printf '%s\n' "${password_files[@]}" | dmenu  -i -p "Pass  : " "$@" -sb "#34495E" -fn "JetBrainsMono Nerd Font")
 
 [[ -n $password ]] || exit
 
