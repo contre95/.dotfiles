f06b3f6b730b79db567a0c9d2c92dce7910b0d24
contre95 <lucascontre95@gmail.com>
2023-08-06 15:34:27 +0200

    Update 06-08-23

--- Scripts/audio-scr/mic-mute.sh
+++ Scripts/audio-scr/mic-mute.sh
@@ -3,3 +3,6 @@
 notify-send "Mic Monitor" "muted toggle" -i "$MY_FOLDER"/Library/icons/muted.png
 pactl set-source-mute 0 toggle # 0 is default
 
+# Or with wireplumber
+# wpctl set-mute $(wpctl status | grep -i 'C01U Pro condenser microphone Analog Stereo' | awk -F' ' '{print $3}' | tr -d '.') toggle
+
