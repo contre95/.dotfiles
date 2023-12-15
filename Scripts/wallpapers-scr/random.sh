8b30d817795a440959d0b6fd6c03caac587aa063
Contre <lucascontre95@gmail.com>
2023-12-15 04:33:11 +0100

    Update 15-12-23

--- Scripts/wallpapers-scr/random.sh
+++ Scripts/wallpapers-scr/random.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 # This script will randomly go through the files of a directory, setting it
 # up as the wallpaper at regular intervals
@@ -13,11 +13,11 @@ if [[ $# -lt 1 ]] || [[ ! -d $1   ]]; then
 fi
 
 # Edit below to control the images transition
-export SWWW_TRANSITION_FPS=60
+export SWWW_TRANSITION_FPS=120
 export SWWW_TRANSITION_STEP=2
 
 # This controls (in seconds) when to switch to the next image
-INTERVAL=300
+INTERVAL=0
 
 while true; do
 	find "$1" \
@@ -26,7 +26,9 @@ while true; do
 		done \
 		| sort -n | cut -d':' -f2- \
 		| while read -r img; do
-			swww img "$img"
+			# swww img "$img"
+            swww img --transition-type grow "$img" 
 			sleep $INTERVAL
 		done
 done
+
