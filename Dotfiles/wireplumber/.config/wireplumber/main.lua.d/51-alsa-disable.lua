2a35a281ba0cc0d5d4c42bea5d97c451065b3557
Contre <lucascontre95@gmail.com>
2024-02-20 20:11:13 +0100

    Update 20-02-24

--- Dotfiles/wireplumber/.config/wireplumber/main.lua.d/51-alsa-disable.lua
+++ Dotfiles/wireplumber/.config/wireplumber/main.lua.d/51-alsa-disable.lua
@@ -2,6 +2,7 @@ rule = {
   matches = {
     {
       { "device.bus-path", "equals", "pci-0000:0a:00.1" },
+      { "device.name", "equals", "lsa_card.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.3" },
     },
   },
   apply_properties = {
