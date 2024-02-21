19deaf6970c7e79eeab463ae735f0e09469f241c
contre95 <lucascontre95@gmail.com>
2024-02-21 22:52:56 +0100

    Crontab sudo backup

--- SmallBinaries/download_binaries.sh
+++ SmallBinaries/download_binaries.sh
@@ -3,5 +3,5 @@
 
 # http-cmd
 rm -f http-cmd &
-wget https://github.com/contre95/http-cmd/releases/latest/download/http-cmd
+wget https://github.com/contre95/http-cmd/releases/latest/download/http-cmd &&
 chmod +x http-cmd
