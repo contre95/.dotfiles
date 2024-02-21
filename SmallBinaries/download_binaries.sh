c8b74ae3c31cbd2a9a157ec4dbfd043b532af43b
contre95 <lucascontre95@gmail.com>
2024-02-21 22:51:28 +0100

    http-cmd

--- SmallBinaries/download_binaries.sh
+++ SmallBinaries/download_binaries.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-curl -O https://github.com/contre95/http-cmd/releases/latest/download/http-cmds
+
+# http-cmd
+rm -f http-cmd &
+wget https://github.com/contre95/http-cmd/releases/latest/download/http-cmd
+chmod +x http-cmd
