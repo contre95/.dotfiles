c6d09c3370b08541f541f5c00330661c9fd0ec45
Contre <lucascontre95@gmail.com>
2024-03-21 22:08:38 +0100

    Update 21-03-24

--- Scripts/waybar-scr/mic-status.sh
+++ Scripts/waybar-scr/mic-status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-if wpctl status | grep "C01U Pro condenser microphone Analog Stereo" | grep "MUTED" >/dev/null 2>&1; then
+if wpctl status | grep "MUTED" | grep "*" >/dev/null 2>&1; then
   echo '<span color="#FF6363"> </span>' "$(pactl get-source-volume 0 | awk -F/ '{ print $2}' | tr -d ' ')"
 else
   echo '<span color="#6D6D6D"> </span>' "$(pactl get-source-volume 0 | awk -F/ '{ print $2}' | tr -d ' ')"
