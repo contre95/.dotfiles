5c474f433a57328ee795cd85bef9b7a3f435c27c
contre95 <lucascontre95@gmail.com>
2024-05-04 20:29:05 +0200

    I'm no state to write a proper commit message

--- Scripts/dmenu-scr/picker.sh
+++ Scripts/dmenu-scr/picker.sh
@@ -1,18 +1,16 @@
-#!/bin/zsh
+#!/bin/bash
 
-"""
-Picker script receives as first arguments files with the following format 
+# Picker script receives as first arguments files with the following format 
+#
+# Name,value
+# ------
+# For example:
+# Google,https://google.com
+# Drive,http://drive.google.com
+# Google Photos,http://photos.google.com
 
-Name2 - value2
-Name  - value
-------
-For example:
-Google    | https://google.com
-Drive     | http://drive.google.com
-Photos    | http://photos.google.com
-"""
-choices="$(cat $1 | tr -d ' ' | awk -F'|' '{print $1}')"
-var="$(echo -e $choices | rofi -dmenu -theme $HOME/.config/rofi/dmenu.style.rasi -i -p 'Pick: ' -sb '#7681C5' -fn 'JetBrainsMono Nerd Font')"
-picked="$(grep $var $1 | awk -F'|' '{print $2}')"
-
-echo $picked
+choices=$(awk -F',' '{print $1}' < "$1")
+var="$(echo -e "$choices" | rofi -dmenu -theme "$HOME"/.config/rofi/dmenu.style.rasi -i -p 'Pick: ' -sb '#7681C5' -fn 'JetBrainsMono Nerd Font')"
+picked=$(grep "$var" "$1" | awk -F',' '{print $2}')
+# echo "$(zenity --entry --text='Bookmark:'),$(zenity --entry --text='URL:')" >>"$MY_FOLDER"/Info/bookmarks.txt
+echo "$picked"
