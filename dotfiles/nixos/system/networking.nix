b0cbe07fc45321bec0cb96cfb25861aa3dd2204f
Contre <lucascontre95@gmail.com>
2024-06-01 06:44:05 +0200

    Networking

--- dotfiles/nixos/system/networking.nix
+++ dotfiles/nixos/system/networking.nix
@@ -5,8 +5,17 @@
   networking.useNetworkd = true;
   networking.networkmanager.enable = false;
   systemd.network.enable = true;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 9100 ];
+    # allowedUDPPortRanges = [
+    #   { from = 4000; to = 4007; }
+    #   { from = 8000; to = 8010; }
+    # ];
+  };
 }
