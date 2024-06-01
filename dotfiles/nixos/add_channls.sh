10e5d85ae5afa72d666361cf4b7b8920b7b86ffe
Contre <lucascontre95@gmail.com>
2024-06-01 14:29:17 +0200

    Networking

--- dotfiles/nixos/add_channls.sh
+++ dotfiles/nixos/add_channls.sh
@@ -1,3 +1,4 @@
-sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz home-manager 
-sudo nix-channel --add https://nixos.org/channels/nixos-23.11 nixos 
+#!/usr/bin/env bash
+sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz home-manager 
+sudo nix-channel --add https://nixos.org/channels/nixos-24.05 nixos 
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable 
