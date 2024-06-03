7d8419f902631e1eed5897974ecc5c0d7fb8ad31
Contre <lucascontre95@gmail.com>
2024-06-03 22:13:06 +0200

    Container podman nix

--- dotfiles/nixos/system/openssh.nix
+++ dotfiles/nixos/system/openssh.nix
@@ -16,20 +16,9 @@
       PermitRootLogin = "no";
       KbdInteractiveAuthentication = false;
     };
-
-    extraConfig = ''
-      Match Address 192.168.0.170/32
-        PasswordAuthentication yes
-      Match Address 192.168.0.172/32,10.8.0.0/24
-        PasswordAuthentication yes
-    '';
-  };
-  users.users."contre".openssh.authorizedKeys.keys = [
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDDerFiqrReOVcinSCWlIoGG31zGGDtnPKiz919RQvEmQTZ6hir7aVQB6/wsNGUse+YEXYoihNdNKkxiXBUS5CJ3w9fc4y4vm6/Q4TQxqqAZg03D+o9UC7009z2YPyz+4pF+74OiPhXLv9JWtVvzscc33NbO9/ipzU+Gi1KmVx7S1ma5dvckaduar4PttqGjxaZ2KDpbu+ZXtsoLLJPWSzpKvUaLnXLY3ZfZtT4jxQZ3UyFFMmyf4Ljq+o8oQctiQbW+or/DN1WwgpVDh3CwMu7jBle8XrfQpINqJVKVR7y13Hy+EifIwNKD1aHKD4MV4wjFSxihNiSy3CUhsrrBxrzu8+RWRKk5dV13f+YU5xBjhvVwqA5mTFtzf6HfznjETtIPGxS1wQGAK+G+2wDzzGYvdOjIwAKfQIrrE7o08JoQ8jewJPwpi9J6XHgwPTfgZk1187Zg9seKWWWKLP9q1IqDW1er1QGEtOr03dU+G8puY5EJQnoSoQ+0/DRiG7MBGkBbSn0wJS4RDGLroDGSR8RWgB3DpnQ3TKc/Y8eCj6+492WXhNMFSKSshzI7tUo0q5hE3vQxabYgcwAxJ2uFKbAf4c7zXnq5Y4czkRj7FXFZcXCHfktVh3Svsu9GQCLJ4tuJbUYGjI2hiqiPMCrugqdtM69OmdnP+uXfh2ghb+GPQ== contre"
-  ];
-  banner =
-    if config.networking.hostName == "notebook" then
-      "
+    banner =
+      if config.networking.hostName == "notebook" then
+        "
                            .
                           | \/|
                           ) )|/|
@@ -46,8 +35,8 @@
                   \   '.
       notebook     \ \ '.)a
    "
-    else if config.networking.hostName == "desktop" then
-      "
+      else if config.networking.hostName == "desktop" then
+        "
              _.-````'-,_
    _,.,_ ,-'`           `'-.,_
  /)     (\                   '``-.
@@ -61,8 +50,8 @@
                 \( \(     |/   |/
       desktop   /_(/_(    /_(  /_(
    "
-    else if config.networking.hostName == "server" then
-      "
+      else if config.networking.hostName == "server" then
+        "
                           .' `'.__
                          /      \ `'`-,
         .-''''--...__..-/ .     |      \
@@ -80,7 +69,18 @@
    \  ,,/ |    \   D    .'  \
     `''`   \  nnh  D_.-'L__nnh    server
     "
-    else "";
+      else "";
+
+    extraConfig = ''
+      Match Address 192.168.0.170/32
+        PasswordAuthentication yes
+      Match Address 192.168.0.172/32,10.8.0.0/24
+        PasswordAuthentication yes
+    '';
+  };
+  users.users."contre".openssh.authorizedKeys.keys = [
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDDerFiqrReOVcinSCWlIoGG31zGGDtnPKiz919RQvEmQTZ6hir7aVQB6/wsNGUse+YEXYoihNdNKkxiXBUS5CJ3w9fc4y4vm6/Q4TQxqqAZg03D+o9UC7009z2YPyz+4pF+74OiPhXLv9JWtVvzscc33NbO9/ipzU+Gi1KmVx7S1ma5dvckaduar4PttqGjxaZ2KDpbu+ZXtsoLLJPWSzpKvUaLnXLY3ZfZtT4jxQZ3UyFFMmyf4Ljq+o8oQctiQbW+or/DN1WwgpVDh3CwMu7jBle8XrfQpINqJVKVR7y13Hy+EifIwNKD1aHKD4MV4wjFSxihNiSy3CUhsrrBxrzu8+RWRKk5dV13f+YU5xBjhvVwqA5mTFtzf6HfznjETtIPGxS1wQGAK+G+2wDzzGYvdOjIwAKfQIrrE7o08JoQ8jewJPwpi9J6XHgwPTfgZk1187Zg9seKWWWKLP9q1IqDW1er1QGEtOr03dU+G8puY5EJQnoSoQ+0/DRiG7MBGkBbSn0wJS4RDGLroDGSR8RWgB3DpnQ3TKc/Y8eCj6+492WXhNMFSKSshzI7tUo0q5hE3vQxabYgcwAxJ2uFKbAf4c7zXnq5Y4czkRj7FXFZcXCHfktVh3Svsu9GQCLJ4tuJbUYGjI2hiqiPMCrugqdtM69OmdnP+uXfh2ghb+GPQ== contre"
+  ];
 
 
 }
