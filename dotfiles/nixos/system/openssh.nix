a105ae3213fac5ab60a528a13d1e351a0613d7e8
Contre <lucascontre95@gmail.com>
2024-06-03 22:30:43 +0200

    ASCII ART SSH

--- dotfiles/nixos/system/openssh.nix
+++ dotfiles/nixos/system/openssh.nix
@@ -16,6 +16,13 @@
       PermitRootLogin = "no";
       KbdInteractiveAuthentication = false;
     };
+    extraConfig = "
+      Match Address 192.168.0.170/32
+        PasswordAuthentication yes
+      Match Address 192.168.0.172/32,10.8.0.0/24
+        PasswordAuthentication yes
+    ";
+
     banner =
       if config.networking.hostName == "notebook" then
         "
@@ -29,7 +36,7 @@
    '.______ (         ) ) \
      '..____ '._       )  )
         .' __.--\  , ,  // ((
-        '.'  mrf|  \/   (_.'(  
+        '.'  ---|  \/   (_.'(  
                 '   \ .' 
                  \   (
                   \   '.
@@ -69,18 +76,12 @@
     /  '/\    /       )    |/     `-...-`
    /    | |  `\    /-'    /;
    \  ,,/ |    \   D    .'  \
-    `''`   \  nnh  D_.-'L__nnh    server
+    `''`   \__nnh  D_.-'L__nnh    server
 
 "
       else "";
-
-    extraConfig = ''
-      Match Address 192.168.0.170/32
-        PasswordAuthentication yes
-      Match Address 192.168.0.172/32,10.8.0.0/24
-        PasswordAuthentication yes
-    '';
   };
+
   users.users."contre".openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDDerFiqrReOVcinSCWlIoGG31zGGDtnPKiz919RQvEmQTZ6hir7aVQB6/wsNGUse+YEXYoihNdNKkxiXBUS5CJ3w9fc4y4vm6/Q4TQxqqAZg03D+o9UC7009z2YPyz+4pF+74OiPhXLv9JWtVvzscc33NbO9/ipzU+Gi1KmVx7S1ma5dvckaduar4PttqGjxaZ2KDpbu+ZXtsoLLJPWSzpKvUaLnXLY3ZfZtT4jxQZ3UyFFMmyf4Ljq+o8oQctiQbW+or/DN1WwgpVDh3CwMu7jBle8XrfQpINqJVKVR7y13Hy+EifIwNKD1aHKD4MV4wjFSxihNiSy3CUhsrrBxrzu8+RWRKk5dV13f+YU5xBjhvVwqA5mTFtzf6HfznjETtIPGxS1wQGAK+G+2wDzzGYvdOjIwAKfQIrrE7o08JoQ8jewJPwpi9J6XHgwPTfgZk1187Zg9seKWWWKLP9q1IqDW1er1QGEtOr03dU+G8puY5EJQnoSoQ+0/DRiG7MBGkBbSn0wJS4RDGLroDGSR8RWgB3DpnQ3TKc/Y8eCj6+492WXhNMFSKSshzI7tUo0q5hE3vQxabYgcwAxJ2uFKbAf4c7zXnq5Y4czkRj7FXFZcXCHfktVh3Svsu9GQCLJ4tuJbUYGjI2hiqiPMCrugqdtM69OmdnP+uXfh2ghb+GPQ== contre"
   ];
