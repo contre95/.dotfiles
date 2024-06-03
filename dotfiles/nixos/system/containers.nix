8f5bca08eebfdbe399e0e1676fe05614fe308ea3
Contre <lucascontre95@gmail.com>
2024-06-03 22:30:43 +0200

    ASCII ART SSH

--- dotfiles/nixos/system/containers.nix
+++ dotfiles/nixos/system/containers.nix
@@ -7,12 +7,27 @@
       enable = true;
 
       # Create a `docker` alias for podman, to use it as a drop-in replacement
+
+      # podman.extraPackages = [ pkgs.zfs ];
       dockerCompat = true;
 
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
     };
+
+    containers = {
+      # storage.settings = {
+      #   storage.driver = "zfs";
+      #   storage.graphroot = "/var/lib/containers/storage";
+      #   storage.runroot = "/run/containers/storage";
+      #   storage.options.zfs.fsname = "zroot/podman";
+      # };
+      containersConf.settings = {
+        containers.dns_servers = [ "8.8.8.8" "8.8.4.4" ];
+      };
+    };
   };
+
 
   # Useful otherdevelopment tools
   environment.systemPackages = with pkgs; [
