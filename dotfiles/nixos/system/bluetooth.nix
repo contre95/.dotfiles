d0b1bc64311886ba3a4f5c6daf303a59db53f006
Contre <lucascontre95@gmail.com>
2024-06-04 20:19:51 +0200

    :snowflake:

--- dotfiles/nixos/system/bluetooth.nix
+++ dotfiles/nixos/system/bluetooth.nix
@@ -10,6 +10,15 @@
   hardware.bluetooth.powerOnBoot = false;
   hardware.bluetooth.settings = {
     General = {
+      # # make Xbox Series X controller work
+      # Class = "0x000100";
+      # ControllerMode = "bredr";
+      # FastConnectable = true;
+      # JustWorksRepairing = "always";
+      # Privacy = "device";
+      Enable = "Source,Sink,Media,Socket";
+      AutoEnable = true;
+      ControllerMode = "bredr";
       Experimental = true;
     };
   };
