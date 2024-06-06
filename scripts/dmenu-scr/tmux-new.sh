5e4130ed10271dd188283beddad1a52be21ee841
Contre <lucascontre95@gmail.com>
2024-06-06 14:11:29 +0200

    Script for script

--- scripts/dmenu-scr/tmux-new.sh
+++ scripts/dmenu-scr/tmux-new.sh
@@ -2,7 +2,7 @@
 
 choices=" RSS\n Server\n Music\n N26\n Android\n Anoux\n Mac\n Remarkable"
 var=$(echo -e $choices | rofi -dmenu -theme $HOME/.config/rofi/dmenu.style.rasi -i -p "Exit menu: " -sb "#2f8a47" -fn "JetBrainsMono Nerd Font")
-TMUX_SCRIPTS=/home/canus/Scripts/tmux-scr
+TMUX_SCRIPTS=/home/canus/scripts/tmux-scr
 
 case $var in
 
