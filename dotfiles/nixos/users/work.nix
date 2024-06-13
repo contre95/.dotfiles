3ff06fe7efc40fac99581effb8e4676f20defd68
Contre <lucascontre95@gmail.com>
2024-06-13 23:05:34 +0200

    Github Copilot :github:

--- dotfiles/nixos/users/work.nix
+++ dotfiles/nixos/users/work.nix
@@ -1,4 +1,14 @@
 { pkgs, ... }: {
+  users.users.contre.home = "/Users/contre";
+  environment = {
+    shells = with pkgs; [ zsh ];
+    darwinConfig = "/Users/contre/dotfiles/nixos/configuration.nix";
+    loginShell = pkgs.zsh;
+    systemPackages = [ pkgs.coreutils ];
+    systemPath = [ "/opt/homebrew/bin" ];
+    pathsToLink = [ "/Applications" ];
+  };
+
   homebrew = {
     brewPrefix = "/opt/homebrew/bin";
     enable = true;
