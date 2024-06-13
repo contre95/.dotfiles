91985a4528de011ed6de99e265c2a1d0e31e5d83
Contre <lucascontre95@gmail.com>
2024-06-13 22:47:48 +0200

    Github Copilot :github:

--- dotfiles/nixos/configuration.nix
+++ dotfiles/nixos/configuration.nix
@@ -25,6 +25,7 @@ in
       ./system/fonts.nix
       ./system/containers.nix
       ./system/networking.nix
+      ./system/version.nix
       ./system/node-exporter.nix
       ./machines/${machineConfig}
       (import "${home-manager}/nixos")
@@ -63,7 +64,7 @@ in
 
 
   # Enable/Disable deafult system programs 
-  programs.nano.enable = false;
+  # programs.nano.enable = false;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users =
@@ -76,14 +77,4 @@ in
         extraGroups = [ "wheel" ];
       };
     } else { };
-
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
-
 }
