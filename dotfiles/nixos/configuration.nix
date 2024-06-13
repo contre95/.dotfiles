e68a749b6b0d9d36eecd36e4fe53f60b4c943c47
Contre <lucascontre95@gmail.com>
2024-06-13 22:49:51 +0200

    Github Copilot :github:

--- dotfiles/nixos/configuration.nix
+++ dotfiles/nixos/configuration.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, ... }:
+{  lib, pkgs, ... }:
 let
   # Machine and environemnt definition
   home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz";
