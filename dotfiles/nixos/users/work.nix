ec5c2ce05bc76dc14bdfd033458208676df4fa5f
Contre <lucascontre95@gmail.com>
2024-06-13 23:18:45 +0200

    Github Copilot :github:

--- dotfiles/nixos/users/work.nix
+++ dotfiles/nixos/users/work.nix
@@ -1,5 +1,61 @@
 { pkgs, ... }: {
-  users.users.contre.home = "/Users/contre";
+
+  imports = [ <home-manager/nix-darwin> ];
+    home-manager.users.contre = {
+    home.packages = [  ];
+      };
+
+   home = {
+    username = "rich";
+    homeDirectory = "/Users/rich";
+    packages = with pkgs; [
+      # Languages
+      go
+      jq
+      cargo
+      python3
+      nodejs_22
+      terraform
+
+      # LSP
+      sqls
+      black
+      gopls
+      pyright
+      nil
+      tfsec
+      tflint
+      terraform-ls
+      nixpkgs-fmt
+      yamlfmt
+      marksman
+      efm-langserver
+      lua-language-server
+
+      # Essentials User 
+      unzip
+      unrar
+      stow
+      bandwhich
+      autojump
+      awscli
+      tree
+      zbar
+      tree-sitter
+      kubectl
+      tmux
+      tmuxPlugins.tmux-thumbs
+      (pass.withExtensions (ext: with ext; [ pass-checkup pass-otp ]))
+      gnupg
+      mpv
+      git
+      neovim
+
+      # Desktop apps
+      alacritty
+
+    ];
+
   environment = {
     shells = with pkgs; [ zsh ];
     darwinConfig = "/Users/contre/dotfiles/nixos/configuration.nix";
@@ -9,25 +65,5 @@
     pathsToLink = [ "/Applications" ];
   };
 
-  homebrew = {
-    brewPrefix = "/opt/homebrew/bin";
-    enable = true;
-    caskArgs.no_quarantine = true;
-    global = {
-      brewfile = true;
-
-    };
-    casks = [
-      "slack"
-      "zoom"
-      "1password"
-      "goland"
-      "pycharm"
-      "rectangle"
-      "notion"
-      "notion-calendar"
-      "viscosity"
-      "bruno"
-    ];
-  };
+};
 }
