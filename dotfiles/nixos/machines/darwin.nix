0ff5b1ba934b62d5457c2b2995db0976284af73a
Contre <lucascontre95@gmail.com>
2024-06-13 23:05:34 +0200

    Github Copilot :github:

--- dotfiles/nixos/machines/darwin.nix
+++ dotfiles/nixos/machines/darwin.nix
@@ -1,4 +1,31 @@
-{... }: {
+{ ... }: {
 
+  nix.extraOptions = ''
+    experimental-features = nix-command
+  '';
+
+  system = {
+    defaults = {
+      NSGlobalDomain = {
+        AppleInterfaceStyle = "Dark";
+        AppleShowAllExtensions = true;
+
+        # Hide the top menu bar
+        _HIHideMenuBar = false;
+      };
+      dock = {
+        autohide = true;
+        orientation = "bottom";
+        show-recents = false;
+      };
+      finder = {
+        AppleShowAllExtensions = true;
+        _FXShowPosixPathInTitle = true;
+      };
+    };
+  };
+
+  services.nix-daemon.enable = true;
+  system.stateVersion = 4;
 }
 
