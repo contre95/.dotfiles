a64638d2ebd9012d8dd5780bef355186f57501cd
Contre <lucascontre95@gmail.com>
2024-06-29 20:52:06 +0200

    Update 29-06-24

--- dotfiles/nixos/system/sound.nix
+++ dotfiles/nixos/system/sound.nix
@@ -8,49 +8,50 @@
     pw-volume
   ];
 
-    sound.enable = true;
-    security.rtkit.enable = true;
+  sound.enable = true;
+  security.rtkit.enable = true;
 
-    services.pipewire = {
-      systemWide = false;
-      audio.enable = true;
-      enable = true;
-      alsa.enable = true;
-         socketActivation = true;
-      pulse.enable = true;
-      wireplumber.enable = true;
-      alsa.support32Bit = true;
-      # 24.05
-      extraConfig.pipewire-pulse."92-low-latency" = {
-        context.modules = [
-          {
-            name = "libpipewire-module-protocol-pulse";
-            args = {
-              pulse.min.req = "32/48000";
-              pulse.default.req = "32/48000";
-              pulse.max.req = "32/48000";
-              pulse.min.quantum = "32/48000";
-              pulse.max.quantum = "32/48000";
-            };
-          }
-        ];
-        stream.properties = {
-          node.latency = "32/48000";
-          resample.quality = 1;
-        };
+  services.pipewire = {
+    systemWide = false;
+    audio.enable = true;
+    enable = true;
+    alsa.enable = true;
+    socketActivation = true;
+    pulse.enable = true;
+    wireplumber.enable = true;
+    alsa.support32Bit = true;
+    # 24.05
+    extraConfig.pipewire-pulse."92-low-latency" = {
+      context.modules = [
+        {
+          name = "libpipewire-module-protocol-pulse";
+          args = {
+            pulse.min.req = "32/48000";
+            pulse.default.req = "32/48000";
+            pulse.max.req = "32/48000";
+            pulse.min.quantum = "32/48000";
+            pulse.max.quantum = "32/48000";
+          };
+        }
+      ];
+      stream.properties = {
+        node.latency = "32/48000";
+        resample.quality = 1;
       };
-
-      # Bluetooth
-      wireplumber.extraConfig = {
-        "monitor.bluez.properties" = {
-          "bluez5.enable-sbc-xq" = true;
-          "bluez5.enable-msbc" = true;
-          "bluez5.enable-hw-volume" = true;
-          "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
-        };
-      };
-
     };
+
+    # Bluetooth
+    wireplumber.extraConfig = {
+      "monitor.bluez.properties" = {
+        "bluez5.enable-sbc-xq" = true;
+        "bluez5.enable-msbc" = true;
+        "bluez5.auto-connect" = [ "hfp_hf" "hsp_hs" "a2dp_sink" "hfp_ag" "hsp_ag"];
+        "bluez5.enable-hw-volume" = true;
+        "bluez5.roles" = [ "a2dp_sink" "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+      };
+    };
+
+  };
 
   # Bluetooth config
   # environment.etc = {
