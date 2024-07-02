c6c5aed9deef053e177c41aed4c907f91a70c290
Contre <lucascontre95@gmail.com>
2024-07-02 16:04:37 +0200

    Steam on notebook

--- dotfiles/nixos/machines/notebook.nix
+++ dotfiles/nixos/machines/notebook.nix
@@ -1,26 +1,32 @@
 { config, lib, pkgs, modulesPath, home-manager, ... }:
 
 {
-
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
     loader.efi.canTouchEfiVariables = true;
-      loader.systemd-boot = {
-        enable = true;
-        # devices = [ "nodev" ];
-        # efiSupport = true;
-        configurationLimit = 10;
-        extraEntries = {
-          "audiotfix.conf" = ''
-            title Audiofix
-            linux /ssdt-csc3551.aml
-          '';
-        };
-
+    loader.systemd-boot = {
+      enable = true;
+      # devices = [ "nodev" ];
+      # efiSupport = true;
+      configurationLimit = 10;
+      extraEntries = {
+        "audiotfix.conf" = ''
+          title Audiofix
+          linux /ssdt-csc3551.aml
+        '';
       };
+
     };
+  };
   # Environment Packages
-  environment.systemPackages = with pkgs; [ acpi ];
+  environment.systemPackages = with pkgs; [ acpi brightnessctl ];
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    gamescopeSession.enable = true;
+  };
+
 
   # # User specific
   # home-manager.users.contre = { pkgs, ... }: {
