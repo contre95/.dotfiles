06b639aedf27919a1bdacfa1aaf4d1cc0eac5286
Contre <lucascontre95@gmail.com>
2024-07-03 10:13:12 +0200

    Steam on notebook

--- dotfiles/nixos/machines/notebook.nix
+++ dotfiles/nixos/machines/notebook.nix
@@ -19,7 +19,8 @@
     };
   };
   # Environment Packages
-  environment.systemPackages = with pkgs; [ acpi brightnessctl ];
+  environment.systemPackages = with pkgs; [ acpi ];
+
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
@@ -27,11 +28,26 @@
     gamescopeSession.enable = true;
   };
 
+  nixpkgs.config.packageOverrides = pkgs: {
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+  };
+  hardware = {
+    opengl.enable = true;
+    opengl.driSupport = true;
+    opengl.driSupport32Bit = true;
+    opengl.extraPackages = with pkgs; [
+      intel-media-driver
+      intel-vaapi-driver
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
 
   # # User specific
-  # home-manager.users.contre = { pkgs, ... }: {
-  #   home.packages = with pkgs; [
-  #   ];
-  # };
+  home-manager.users.contre = { pkgs, ... }: {
+    home.packages = with pkgs; [
+      brightnessctl
+    ];
+  };
 
 }
