6f14275e36a699bc4f8743b739870f70910fbe60
Contre <lucascontre95@gmail.com>
2024-07-10 23:50:52 +0200

    Update 10-07-24

--- scripts/dmenu-scr/shaders.sh
+++ scripts/dmenu-scr/shaders.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 shadersPath=~/.config/hypr/shaders
-shader="$(find $shadersPath -printf '%f\n' | rofi -dmenu -theme $HOME/.config/rofi/dmenu.style.rasi -i -p 'Shader: ' -sb '#D96E1F' -fn 'JetBrainsMono Nerd Font')"
-
-if [[ ! $shader -eq "" ]]; then
-    echo "$shadersPath/$shader"
-	hyprctl keyword decoration:screen_shader "$shadersPath/$shader"
+shader="$(ls -1 $shadersPath | rofi -dmenu -theme $HOME/.config/rofi/dmenu.style.rasi -i -p 'Shader: ' -sb '#D96E1F' -fn 'JetBrainsMono Nerd Font')"
+if [[ ! -z $shader ]]; then
+    hyprctl keyword decoration:screen_shader "$shadersPath/$shader"
 else
-	hyprctl keyword decoration:screen_shader "[[EMPTY]]" 
+    hyprctl keyword decoration:screen_shader "[[EMPTY]]"
 fi
+
