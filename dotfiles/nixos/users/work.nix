0cc0c16b176dbb26c4b2c8ea088820c55b832157
Contre <lucascontre95@gmail.com>
2024-07-18 15:27:31 +0200

    gd git diff

--- dotfiles/nixos/users/work.nix
+++ dotfiles/nixos/users/work.nix
@@ -73,7 +73,7 @@
 
     home.sessionVariables = {
       MY_FOLDER = "/Users/canus";
-      PATH = "$PATH:/Users/lucas.contreras/.nix-profile/bin:/run/current-system/sw/bin:/nix/var/nix/profiles/default/bin:$HOME/.pyenv/bin:/go/bin/";
+      PATH = "$PATH:/Users/lucas.contreras/.nix-profile/bin:/run/current-system/sw/bin:/nix/var/nix/profiles/default/bin:$HOME/.pyenv/bin:/go/bin/:/usr/local/bin";
       NIX_PATH = "/Users/lucas.contreras/.nix-defexpr/channels:darwin-config=/Users/canus/dotfiles/nixos/configuration.nix:/nix/var/nix/profiles/per-user/root/channels";
       SCR_PATH = "$MY_FOLDER/scripts";
       EDITOR = "nvim";
