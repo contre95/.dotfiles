c07dc535351d439dd9e2f5b8ac7dc303f7001eeb
Contre <lucascontre95@gmail.com>
2024-08-22 19:33:21 +0200

    Notebook

--- dotfiles/nixos/machines/notebook.nix
+++ dotfiles/nixos/machines/notebook.nix
@@ -42,13 +42,6 @@
   # Environment Packages
   environment.systemPackages = with pkgs; [ acpi ];
 
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    gamescopeSession.enable = true;
-  };
-
   nixpkgs.config.packageOverrides = pkgs: {
     intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
   };
