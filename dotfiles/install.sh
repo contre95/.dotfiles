bfeeae2396232d286874f0976af3b62eeafc2213
Contre <lucascontre95@gmail.com>
2024-09-08 01:31:19 +0200

    server

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env bash
-echo "Remember to create the machines/<machine_name>.nix and add machine to configuration.nix" 
-sleep 3
-nix-shell -p gpg git 
-sudo mkdir /home/canus
-sudo chown contre /home/canus
-pushd /home/canus/
-gpg --import  ./nixos/contre.pub
-gpg --card-status
-git clone git@github.com/contre95/.dotfiles.git /home/canus/
+echo "Remember to:
+1. Create the machines/<machine_name>.nix and add machine to configuration.nixi
+2. Have your GPG smart card connected.
+3. Have interenet connection.i
+4. nix-shell -p git gnupg vim"
+sleep 3 &&
+sudo rm -rf /home/canus &&
+sudo mkdir /home/canus &&
+sudo chown contre /home/canus &&
+pushd /home/canus/ &&
+gpg --import <(curl https://github.com/contre95.gpg)  &&
+gpg --card-status &&
+git config --global user.email lucascontre95@gmail.com
+git clone https://github.com/contre95/.dotfiles.git /home/canus/ &&
+git remote -v &&
+git remote set-url origin git@github.com:contre95/.dotfiles.git
 # Backup /etc/nixos
-sudo cp /etc/nixos/hardware-configuration.nix /home/contre
-sudo cp /etc/nixos/hardware-configuration.nix /home/contre
-# Rebiuld /etc/nixos
-sudo rm -rf /etc/nixos/
-sudo ln -s /etc/nixos/ /home/canus/dotfiles/nixos/
-# Update Channels
-sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz home-manager 
-sudo nix-channel --add https://nixos.org/channels/nixos-24.05 nixos 
-sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable 
-# Prepare and run 
-cp  ./zsh/p10k.zsh /home/contre/.config
+sudo cp /etc/nixos/hardware-configuration.nix /home/contre && \
+sudo rm -rf /etc/nixos/ && sudo mkdir /etc/nixos && \
+sudo find /home/canus/dotfiles/nixos/ -mindepth 1 -maxdepth 1 -exec ln -vs "{}" /etc/nixos ';'
+#sudo ln -s /home/canus/dotfiles/nixos/configuration.nix /etc/nixos/configuration.nix
+#sudo ln -s /home/canus/dotfiles/nixos/machines /etc/nixos/machines
+#sudo ln -s /home/canus/dotfiles/nixos/programs /etc/nixos/programs
+#sudo ln -s /home/canus/dotfiles/nixos/system /etc/nixos/system
+sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz home-manager  && \
+sudo nix-channel --add https://nixos.org/channels/nixos-24.05 nixos  && \
+sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable  && \
+cp  /home/canus/dotfiles/zsh/.p10k.zsh /home/contre/.config && \
+sudo cp /home/contre/hardware-configuration.nix /etc/nixos/hardware-configuration.nix && \
 sudo WHICH_MACHINE=$1 nixos-rebuild switch --upgrade
