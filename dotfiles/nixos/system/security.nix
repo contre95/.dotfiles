454303d14e25615e144b79a0a15b8312c7881f61
Contre <lucascontre95@gmail.com>
2024-09-08 17:05:21 +0200

    GPG + SSH

--- dotfiles/nixos/system/security.nix
+++ dotfiles/nixos/system/security.nix
@@ -1,5 +1,4 @@
 { pkgs, ... }:
 {
-  # openssl s_client -connect <your-server-url>:443 -showcerts </dev/null 2>/dev/null | openssl x509 -outform PEM > server_cert.pem
   security.rtkit.enable = true;
 }
