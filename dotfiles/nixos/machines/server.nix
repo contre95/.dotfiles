7e5ee1b089069b0e9af116592f8e4b151e04ad86
Contre <lucascontre95@gmail.com>
2024-09-08 02:19:01 +0200

    server

--- dotfiles/nixos/machines/server.nix
+++ dotfiles/nixos/machines/server.nix
@@ -1,2 +1,19 @@
 {...}:
-{}
+{
+
+   fileSystems."/mnt/HDD2" = {
+     device = "/dev/sdb1";
+     fsType = "ext4"; # Replace with your filesystem type
+   };
+
+   fileSystems."/mnt/SSD" = {
+     device = "/dev/sda1";
+     fsType = "ext4"; # Replace with your filesystem type
+   };
+
+   fileSystems."/mnt/HDD" = {
+     device = "/dev/sdc1";
+     fsType = "ext4"; # Replace with your filesystem type
+   };
+
+  }
