a4ec910dcf412f775e91913fcc64ab8ebe6a9b5c
Contre <lucascontre95@gmail.com>
2024-09-08 16:55:17 +0200

    GPG + SSH

--- dotfiles/nixos/system/security.nix
+++ dotfiles/nixos/system/security.nix
@@ -2,22 +2,4 @@
 {
   # openssl s_client -connect <your-server-url>:443 -showcerts </dev/null 2>/dev/null | openssl x509 -outform PEM > server_cert.pem
   security.rtkit.enable = true;
-
-  security.sudo = {
-    enable = true;
-    extraRules = [{
-      commands = [
-        {
-          command = "/run/current-system/sw/bin/nixos-rebuild";
-          options = [ "NOPASSWD" ];
-        }
-      ];
-      users = [ "contre" ];
-    }];
-    # extraConfig = with pkgs; ''
-    #   Defaults:nixos-rebuild secure_path="${lib.makeBinPath [
-    #     systemd
-    #   ]}:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
-    # '';
-  };
 }
