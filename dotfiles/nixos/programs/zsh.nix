9fc75fcf6ac1bfcd4ab99dde9efa2faac8df8cee
Contre <lucascontre95@gmail.com>
2024-09-10 22:43:39 +0200

    Update

--- dotfiles/nixos/programs/zsh.nix
+++ dotfiles/nixos/programs/zsh.nix
@@ -42,8 +42,8 @@
 
 
     shellAliases = {
+      rebuild = "sudo WHICH_MACHINE=$(hostname) nixos-rebuild switch";
       update = "pushd $MY_FOLDER && git pull && sudo WHICH_MACHINE=$(hostname) nixos-rebuild switch";
-      update-mac = "sudo WHICH_MACHINE=macbook darwin-rebuild";
       iptr = "sudo iptables -t nat -L -v -n";
       mf = "cd $MY_FOLDER";
       p = "podman --remote";
