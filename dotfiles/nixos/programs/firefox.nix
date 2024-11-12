ef927d5810635af9c06b3c5e04214bb40c9353a4
Contre <lucascontre95@gmail.com>
2024-11-12 21:12:39 +0100

    Nvim

--- dotfiles/nixos/programs/firefox.nix
+++ dotfiles/nixos/programs/firefox.nix
@@ -51,91 +51,83 @@
       ];
 
       settings = {
-        "dom.security.https_only_mode" = true; # force https
-        "browser.download.panel.shown" = true; # show download panel
-        "identity.fxaccounts.enabled" = false; # disable firefox accounts
-        "signon.rememberSignons" = false; # disable saving passwords
-        "extensions.pocket.enabled" = false; # disable pocket
-        "app.shield.optoutstudies.enabled" = false; # disable shield studies
         "app.update.auto" = false; # disable auto update
-        "browser.bookmarks.restore_default_bookmarks" = false; # don't restore default bookmarks
+        "dom.security.https_only_mode" = true; # force https
+        "extensions.pocket.enabled" = false; # disable pocket
         "browser.quitShortcut.disabled" = true; # disable ctrl+q
+        "browser.download.panel.shown" = true; # show download panel
+        "signon.rememberSignons" = false; # disable saving passwords
+        "identity.fxaccounts.enabled" = false; # disable firefox accounts
+        "app.shield.optoutstudies.enabled" = false; # disable shield studies
         "browser.shell.checkDefaultBrowser" = false; # don't check if default browser
+        "browser.bookmarks.restore_default_bookmarks" = false; # don't restore default bookmarks
 
-        # download handling
+        # Download handling
         "browser.download.dir" = "/home/meain/down"; # default download dir
         "browser.startup.page" = 3; # restore previous session
-
-        # ui changes
-        "browser.aboutConfig.showWarning" = false; # disable warning about about:config
-        "browser.compactmode.show" = true; # enable compact mode
+        # UI changes
         "browser.uidensity" = 1; # enable dense UI
         "general.autoScroll" = true; # enable autoscroll
+        "browser.compactmode.show" = true; # enable compact mode
         "browser.tabs.firefox-view" = false; # enable firefox view
-        "browser.toolbars.bookmarks.visibility" = "never"; # hide bookmarks toolbar
-        "media.videocontrols.picture-in-picture.video-toggle.enabled" = true; # disable picture in picture button
         "startup.homepage_welcome_url" = ""; # disable welcome page
         "browser.newtabpage.enabled" = false; # disable new tab page
-        # "toolkit.legacyUserProfileCustomizations.stylesheets" = true; # enable userChrome.css
         "full-screen-api.ignore-widgets" = true; # fullscreen within window
+        "browser.toolbars.bookmarks.visibility" = "never"; # hide bookmarks toolbar
+        "browser.aboutConfig.showWarning" = false; # disable warning about about:config
+        "media.videocontrols.picture-in-picture.video-toggle.enabled" = true; # disable picture in picture button
 
-        # privacy
-        "browser.contentblocking.category" = "custom"; # set tracking protection to custom
+        # Privacy
         "browser.discovery.enabled" = false; # disable discovery
         "browser.search.suggest.enabled" = false; # disable search suggestions
-        "browser.protections_panel.infoMessage.seen" = true; # disable tracking protection info
+        "browser.contentblocking.category" = "custom"; # set tracking protection to custom
         "dom.private-attribution.submission.enabled" = false; # stop doing dumb stuff mozilla
+        "browser.protections_panel.infoMessage.seen" = true; # disable tracking protection info
 
-        # disable telemetry
-        "browser.newtabpage.activity-stream.feeds.telemetry" = false;
-        "browser.translations.automaticallyPopup" = false;
-        "browser.newtabpage.activity-stream.telemetry" = false;
+        # Disable telemetry
+        "toolkit.telemetry.enabled" = false;
+        "toolkit.telemetry.unified" = false;
         "browser.ping-centre.telemetry" = false;
         "toolkit.telemetry.archive.enabled" = false;
         "toolkit.telemetry.bhrPing.enabled" = false;
-        "toolkit.telemetry.enabled" = false;
-        "toolkit.telemetry.firstShutdownPing.enabled" = false;
+        "toolkit.telemetry.updatePing.enabled" = false;
+        "browser.translations.automaticallyPopup" = false;
         "toolkit.telemetry.hybridContent.enabled" = false;
         "toolkit.telemetry.newProfilePing.enabled" = false;
         "toolkit.telemetry.reportingpolicy.firstRun" = false;
+        "toolkit.telemetry.firstShutdownPing.enabled" = false;
+        "browser.newtabpage.activity-stream.telemetry" = false;
         "toolkit.telemetry.shutdownPingSender.enabled" = false;
-        "toolkit.telemetry.unified" = false;
-        "toolkit.telemetry.updatePing.enabled" = false;
+        "browser.newtabpage.activity-stream.feeds.telemetry" = false;
 
         # let me close and open tabs without confirmation
-        "browser.tabs.closeWindowWithLastTab" = false; # don't close window when last tab is closed
-        "browser.tabs.loadBookmarksInTabs" = true; # open bookmarks in new tab
-        "browser.tabs.loadDivertedInBackground" = false; # open new tab in background
         "browser.tabs.loadInBackground" = true; # open new tab in background
-        "browser.tabs.warnOnClose" = false; # don't warn when closing multiple tabs
-        "browser.tabs.warnOnCloseOtherTabs" = false; # don't warn when closing multiple tabs
+        "browser.tabs.loadBookmarksInTabs" = true; # open bookmarks in new tab
         "browser.tabs.warnOnOpen" = false; # don't warn when opening multiple tabs
         "browser.tabs.warnOnQuit" = false; # don't warn when closing multiple tabs
+        "browser.tabs.warnOnClose" = false; # don't warn when closing multiple tabs
+        "browser.tabs.loadDivertedInBackground" = false; # open new tab in background
+        "browser.tabs.warnOnCloseOtherTabs" = false; # don't warn when closing multiple tabs
+        "browser.tabs.closeWindowWithLastTab" = false; # don't close window when last tab is closed
 
         # other
-        "devtools.cache.disabled" = true; # disable caching in devtools
+        "media.autoplay.default" = 0; # enable autoplay on open
         "devtools.toolbox.host" = "right"; # move devtools to right
         # "browser.ssb.enabled" = true; # enable site specific browser
-        "media.autoplay.default" = 0; # enable autoplay on open
-        "media.ffmpeg.vaapi.enabled" = true; # enable hardware acceleration
         "media.rdd-vpx.enabled" = true; # enable hardware acceleration
+        "devtools.cache.disabled" = true; # disable caching in devtools
+        "media.ffmpeg.vaapi.enabled" = true; # enable hardware acceleration
 
-        # override fonts (Set tracking protection to custom without "Suspected fingerprinters")
-        "font.minimum-size.x-western" = 13;
+        # Fonts
         "font.size.fixed.x-western" = 15;
-        "font.size.monospace.x-western" = 15;
+        "font.minimum-size.x-western" = 13;
         "font.size.variable.x-western" = 15;
+        "font.size.monospace.x-western" = 15;
         "browser.display.use_document_fonts" = 0;
-
-        # do not open a tab in a new window
-        # ascentpayroll.net open link in a new without without any
-        # chrome and I can't even use my password manager
-        # https://support.mozilla.org/eu/questions/1151067?&mobile=1
         "browser.link.open_newwindow.restriction" = 0;
 
-        # "keyword.enable" = false; # Disable search when typing unexistent TLD
         "browser.fixup.domainsuffixwhitelist.home" = true;
-        "browser.fixup.domainwhitelist.server.home" = true; # whitelist contre.lucas tld
+        "browser.fixup.domainwhitelist.server.home" = true;
         # "keyword.enable" = false; # Disable search when typing unexistent TLD
       };
 
