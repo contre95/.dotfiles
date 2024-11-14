4f35966599cc8aa2d56f42aeb911b65c423d6733
Lucas Contreras <lucascontre95@gmail.com>
2024-11-14 10:31:42 +0100

    Treesitter

--- dotfiles/nvim/lua/plugins/telescope.lua
+++ dotfiles/nvim/lua/plugins/telescope.lua
@@ -10,5 +10,13 @@ return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.8',
   -- or                              , branch = '0.1.x',
-  dependencies = { 'nvim-lua/plenary.nvim' }
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = {
+    defaults = {
+      file_ignore_patterns = {
+        "node_modules",
+        "vendor",
+      },
+    }
+  }
 }
