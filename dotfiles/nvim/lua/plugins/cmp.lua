efe4e0d85c0a4b11eeabde79d54e29f3f0ade917
Lucas Contreras <lucascontre95@gmail.com>
2024-11-14 20:31:52 +0100

    Change order of test to avoit nil pointer reference

--- dotfiles/nvim/lua/plugins/cmp.lua
+++ dotfiles/nvim/lua/plugins/cmp.lua
@@ -1,119 +1,177 @@
-table.unpack = table.unpack
-local has_words_before = function()
-  local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-local feedkey = function(key, mode)
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-end
-
-local cmp = require("cmp")
-local lspkind = require('lspkind')
-return {
-  {
-    "hrsh7th/nvim-cmp",
-    lazy = true,
-    event = "VeryLazy",
-    priority = 100,
-    dependencies = {
-      {
-        -- snippet plugin
-        "L3MON4D3/LuaSnip",
-        dependencies = "rafamadriz/friendly-snippets",
-        opts = { history = true, updateevents = "TextChanged,TextChangedI" },
-      },
-      -- autopairing of (){}[] etc
-      {
-        "windwp/nvim-autopairs",
-        opts = {
-          fast_wrap = {},
-          disable_filetype = { "TelescopePrompt", "vim" },
+return {  -- Autocompletion
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    -- Snippet Engine & its associated nvim-cmp source
+    {
+      "L3MON4D3/LuaSnip",
+      -- build = (function()
+      --   -- Build Step is needed for regex support in snippets.
+      --   -- This step is not supported in many windows environments.
+      --   -- Remove the below condition to re-enable on windows.
+      --   if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+      --     return
+      --   end
+      --   return "make install_jsregexp"
+      -- end)(),
+      dependencies = {
+        -- `friendly-snippets` contains a variety of premade snippets.
+        --    See the README about individual language/framework/plugin snippets:
+        --    https://github.com/rafamadriz/friendly-snippets
+        {
+          "rafamadriz/friendly-snippets",
+          config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+          end,
         },
-        config = function(_, opts)
-          require("nvim-autopairs").setup(opts)
-
-          -- setup cmp for autopairs
-          local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-          require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-        end,
       },
-      "onsails/lspkind.nvim",
-      "saadparwaiz1/cmp_luasnip",
-      "hrsh7th/cmp-nvim-lua",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
     },
-    opts = {
-      -- Mapping
-      mapping = {
-        ['<CR>'] = cmp.mapping.confirm({
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true,
-        }),
+    "saadparwaiz1/cmp_luasnip",
 
-        ["<S-Tab>"] = cmp.mapping(function()
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-            feedkey("<Plug>(vsnip-jump-prev)", "")
-          end
-        end, { "i", "s" }),
-
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif vim.fn["vsnip#available"](1) == 1 then
-            feedkey("<Plug>(vsnip-expand-or-jump)", "")
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end),
-
-      },
-      -- Windows
+    -- Adds other completion capabilities.
+    --  nvim-cmp does not ship with all sources by default. They are split
+    --  into multiple repos for maintenance purposes.
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-path",
+    -- "zbirenbaum/copilot-cmp",
+  },
+  config = function()
+    local kind_icons = {
+      Text = "",
+      Method = "m",
+      Function = "",
+      Constructor = "",
+      Field = "",
+      Variable = "",
+      Class = "",
+      Interface = "",
+      Module = "",
+      Property = "",
+      Unit = "",
+      Value = "",
+      Enum = "",
+      Keyword = "",
+      Snippet = "",
+      Color = "",
+      File = "",
+      Reference = "",
+      Folder = "",
+      EnumMember = "",
+      Constant = "",
+      Struct = "",
+      Event = "",
+      Operator = "",
+      TypeParameter = "",
+      Copilot = "",
+    }
+    -- See `:help cmp`
+    require("luasnip/loaders/from_vscode").lazy_load()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    luasnip.config.setup({})
+    local check_backspace = function()
+      local col = vim.fn.col(".") - 1
+      return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+    end
+    cmp.setup({
       window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
       },
-
-      -- Sources
-      sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'path' },
-        { name = 'vsnip' },
-        { name = 'copilot', group_index = 2 },
-        { name = 'buffer',  keyword_length = 3, },
-      }),
-
       snippet = {
         expand = function(args)
-          vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+          luasnip.lsp_expand(args.body)
         end,
       },
+      completion = { completeopt = "menu,menuone,noinsert" },
 
+      -- For an understanding of why these mappings were
+      -- chosen, you will need to read `:help ins-completion`
+      --
+      -- No, but seriously. Please read `:help ins-completion`, it is really good!
+      mapping = cmp.mapping.preset.insert({
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+            -- elseif require("copilot.suggestion").is_visible() then
+            --   require("copilot.suggestion").accept()
+          elseif luasnip.expandable() then
+            luasnip.expand()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif check_backspace() then
+            fallback()
+          else
+            fallback()
+          end
+        end, {
+          "i",
+          "s",
+        }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, {
+          "i",
+          "s",
+        }),
+        -- Manually trigger a completion from nvim-cmp.
+        --  Generally you don't need this, because nvim-cmp will display
+        --  completions whenever it has completion options available.
+        ["<C-Space>"] = cmp.mapping.complete({}),
+
+        -- Think of <c-l> as moving to the right of your snippet expansion.
+        --  So if you have a snippet that's like:
+        --  function $name($args)
+        --    $body
+        --  end
+        --
+        -- <c-l> will move you to the right of each of the expansion locations.
+        -- <c-h> is similar, except moving you backwards.
+        ["<C-n>"] = cmp.mapping(function()
+          if luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          end
+        end, { "i", "s" }),
+        ["<C-m>"] = cmp.mapping(function()
+          if luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          end
+        end, { "i", "s" }),
+
+        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+      }),
+      sources = {
+        { name = "nvim_lsp" },
+        -- { name = "copilot" },
+        { name = "buffer" },
+        { name = "luasnip" },
+        { name = "path" },
+        { name = "cmdline" },
+      },
       formatting = {
-        --format = lspkind.cmp_format({with_text = false, maxwidth = 50})
-        format = lspkind.cmp_format {
-          with_text = true,
-          menu = {
-            copilot = "[Copilot]",
-            buffer = "[Buf]",
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+          -- Kind icons
+          vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+          -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+          vim_item.menu = ({
             nvim_lsp = "[LSP]",
+            luasnip = "[Snippet]",
+            -- copilot = "[Copilot]",
+            buffer = "[Buffer]",
             path = "[Path]",
-            vsnip = "[Snip]",
-            gh_issues = "[Issues]",
-            tn = "[TabNine]",
-          },
-        },
+          })[entry.source.name]
+          return vim_item
+        end,
       },
-      experimental = {
-        native_menu = false,
-        ghost_text = false, -- virtual text
-      },
-    },
-  },
+    })
+  end,
 }
--- dotfiles/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -3,11 +3,10 @@ return {
     "nvim-treesitter/nvim-treesitter",
     event = { "BufReadPost", "BufNewFile" },
     cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-    -- build = ":TSUpdate",
+    build = ":TSUpdate",
     dependencies = "windwp/nvim-ts-autotag",
     opts = {
       ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "terraform", "javascript", "html", "python" },
-      sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
     },
