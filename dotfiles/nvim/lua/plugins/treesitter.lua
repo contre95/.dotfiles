ca64cbefb2d6fc9a4ad2fffcf68c9c525382cf3a
Contre <lucascontre95@gmail.com>
2024-11-16 15:05:12 +0100

    null-]s

--- dotfiles/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -3,10 +3,11 @@ return {
     "nvim-treesitter/nvim-treesitter",
     event = { "BufReadPost", "BufNewFile" },
     cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+    lazy = false,
     build = ":TSUpdate",
     dependencies = "windwp/nvim-ts-autotag",
     opts = {
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "terraform", "javascript", "html", "python" },
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "terraform", "javascript", "html", "python", "yaml", "json", "toml", "nix", "sql", ""},
       highlight = { enable = true },
       indent = { enable = true },
     },
