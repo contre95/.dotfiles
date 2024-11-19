92f9f5afc9693313c4b058e38e957f2cd9fddf68
Lucas Contreras <lucascontre95@gmail.com>
2024-11-19 13:48:26 +0100

    Which key

--- dotfiles/nvim/lua/plugins/gopher.lua
+++ dotfiles/nvim/lua/plugins/gopher.lua
@@ -9,14 +9,6 @@ return {
     ft = "go",
     config = function()
       require("gopher").setup()
-      local wk = require("which-key")
-      wk.add({
-        { "<leader>ga",  group = "Add" },
-        { "<leader>gai", "<cmd>GoIfErr<CR>",       desc = "Add if != err statement" },
-        { "<leader>gs",  group = "Struct Tags" },
-        { "<leader>gsj", "<cmd>GoTagAdd json<CR>", desc = "Add json struct tags" },
-        { "<leader>gsy", "<cmd>GoTagAdd yaml<CR>", desc = "Add yaml struct tags" },
-      })
     end,
     build = function()
       vim.cmd [[silent! GoInstallDeps]]
