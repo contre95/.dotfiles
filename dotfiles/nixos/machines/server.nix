a1417ee58188522edd723cc9d737b3064623969e
Contre <lucascontre95@gmail.com>
2024-12-13 21:33:03 +0100

    Server

--- dotfiles/nixos/machines/server.nix
+++ dotfiles/nixos/machines/server.nix
@@ -60,9 +60,25 @@
   };
 
   environment.systemPackages = with pkgs; [
-    motion
     audit
+    lutris
+    mangohud
+    motion
+    nv-codec-headers-12
+    vulkan-loader
+    vulkan-tools
+    wineWowPackages.waylandFull
+    winetricks
   ];
+
+  programs.gamemode.enable = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    gamescopeSession.enable = true;
+  };
+
   fileSystems."/mnt/HDD2" = {
     device = "/dev/disk/by-uuid/b0b93094-b438-458c-97fe-68d4e49bd007";
     fsType = "ext4"; # Replace with your filesystem type
