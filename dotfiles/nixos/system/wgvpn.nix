91f76049a7c6540ce9ae61fc9b5fecd76f7cfc37
Contre <lucascontre95@gmail.com>
2025-03-02 12:32:57 +0100

    Wireguard

--- dotfiles/nixos/system/wgvpn.nix
+++ dotfiles/nixos/system/wgvpn.nix
@@ -1,39 +1,39 @@
-{ ... }:
+{ config, pkgs, ... }:
+{
+  networking.firewall.allowedUDPPorts = [ 51028 ];
+  environment.systemPackages = with pkgs; [ wireguard-tools ];
+  networking.wg-quick.interfaces = {
+    wg0 = {
+      autostart = false;
+      address = [ "10.16.12.93/32" ];
+      dns = [ "10.16.12.1" ];
+      privateKeyFile = "/home/contre/.wg/pk.key";
+
+      peers = [
+        {
+          publicKey = "TkzWsGsHIaWm7Cj9xw5J94q4rMm1unrEGJz6ep5NnRo=";
+          # presharedKeyFile = "/root/wireguard-keys/preshared_from_peer0_key";
+          allowedIPs = [
+            "0.0.0.0/0"
+          ];
+          endpoint = "vpn.contre.io:51028";
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+  };
+security.sudo.extraRules = [
   {
-    networking.firewall = {
-      allowedUDPPorts = [ 51028 ]; # Clients and peers can use the same port, see listenport
-    };
-    # Enable Wireguard
-    networking.wireguard.interfaces = {
-      wg0 = {
-        # Determines the IP address and subnet of the client's end of the tunnel interface.
-        ips = [ "10.16.12.93" ];
-
-        # Path to the private key file.
-        #
-        # Note: The private key can also be included inline via the privateKey option,
-        # but this makes the private key world-readable; thus, using privateKeyFile is
-        # recommended.
-       listenPort = 51028;
-        privateKeyFile = "~/.wg/pk.key";
-
-        peers = [
-          # For a client configuration, one peer entry for the server will suffice.
-          {
-            # Public key of the server (not a file path).
-            publicKey = "TkzWsGsHIaWm7Cj9xw5J94q4rMm1unrEGJz6ep5NnRo=";
-
-            # List of IPs assigned to this peer within the tunnel subnet. Used to configure routing.
-            # For a server peer this should be the whole subnet.
-            allowedIPs = [ "0.0.0.0/0" ];
-
-            # Set this to the server IP and port.
-            endpoint = "79.152.165.88:51820";
-
-            # Send keepalives every 25 seconds. Important to keep NAT tables alive.
-            # persistentKeepalive = 25;
-          }
-        ];
-      };
-    };
+    groups = [ "wheel" ];  # Replace with your actual group (e.g., "users", "sudo")
+    commands = [
+      {
+        command = "/usr/bin/env systemctl start wg-quick-wg0.service";  # Use "@" if your service is templated
+        options = [ "NOPASSWD" ];
+      }
+      {
+        command = "/usr/bin/env systemctl stop wg-quick-wg0.service";
+        options = [ "NOPASSWD" ];
+      }
+    ];
   }
+];}
