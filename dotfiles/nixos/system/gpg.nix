a20218b955965c1f08a44be84ceb7006b6b6f7f3
Contre <lucascontre95@gmail.com>
2025-03-11 13:10:45 +0100

    GPG

--- dotfiles/nixos/system/gpg.nix
+++ dotfiles/nixos/system/gpg.nix
@@ -1,7 +1,6 @@
 {
   pkgs,
   lib,
-  config,
   ...
 }:
 let
@@ -18,18 +17,8 @@ in
       {
         services.pcscd.enable = true;
         services.dbus.packages = [ pkgs.gcr ];
-
         # Enable GPG at a system level
         services.yubikey-agent.enable = true;
-        programs.gnupg.dirmngr.enable = true;
-
-        # programs.gnupg.agent = {
-        #   enable = true;
-        #   enableSSHSupport = true;
-        #   enableExtraSocket = true;
-        #   pinentryPackage = pkgs.pinentry-gnome3;
-        # };
-
         # Enable GPG Smartcards (Like Yubikeys)
         hardware.gpgSmartcards.enable = true;
       }
