17dbfa38df6f35e413d71b8e9dc053dbed15ea32
Contre <lucascontre95@gmail.com>
2025-04-01 13:07:32 +0200

    Nvim 0.10

--- dotfiles/nixos/machines/notebook.nix
+++ dotfiles/nixos/machines/notebook.nix
@@ -65,12 +65,6 @@ in
   nixpkgs.config.packageOverrides = pkgs: {
     intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
   };
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    gamescopeSession.enable = true;
-  };
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
