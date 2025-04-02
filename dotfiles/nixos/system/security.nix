633c84aa0ec3cfdb93005fe47ee7ada3f1d62cb6
Contre <lucascontre95@gmail.com>
2025-04-03 00:12:31 +0200

    New cert for Caddy

--- dotfiles/nixos/system/security.nix
+++ dotfiles/nixos/system/security.nix
@@ -6,20 +6,21 @@
   ];
 
   security.rtkit.enable = true;
+# openssl s_client -connect server.home:443 </dev/null 2>/dev/null | openssl x509 -inform pem -text
   security.pki.certificates = [
     ''
       -----BEGIN CERTIFICATE-----
-      MIIBpDCCAUqgAwIBAgIRAIHxoPlrvKIDe2p9xa4JyVAwCgYIKoZIzj0EAwIwMDEu
-      MCwGA1UEAxMlQ2FkZHkgTG9jYWwgQXV0aG9yaXR5IC0gMjAyNCBFQ0MgUm9vdDAe
-      Fw0yNDA5MDgwMjEyMDFaFw0zNDA3MTgwMjEyMDFaMDAxLjAsBgNVBAMTJUNhZGR5
-      IExvY2FsIEF1dGhvcml0eSAtIDIwMjQgRUNDIFJvb3QwWTATBgcqhkjOPQIBBggq
-      hkjOPQMBBwNCAATlRudHbyrrHsbPlieQn9Jsm+3E1rTx8seoqroC4zAnuvT1Rnkz
-      bkHYoH5Yqm+U88oFFvv9UazN1rYfQi3DGPOSo0UwQzAOBgNVHQ8BAf8EBAMCAQYw
-      EgYDVR0TAQH/BAgwBgEB/wIBATAdBgNVHQ4EFgQUFxGMp2jlbhs4gCNrCztlXbJi
-      QekwCgYIKoZIzj0EAwIDSAAwRQIhAKXqTdwu1KlegREvKWOWbNKstmwgADfwSwno
-      KBzRe5FJAiAgGVlng9FDrihC4oImL9o8VQ1tUAf8GCdPxL4bCtc7MQ==
+      MIIBwDCCAWagAwIBAgIRALGemIvgJcC1EZiaVt15HPYwCgYIKoZIzj0EAwIwMzEx
+      MC8GA1UEAxMoQ2FkZHkgTG9jYWwgQXV0aG9yaXR5IC0gRUNDIEludGVybWVkaWF0
+      ZTAeFw0yNTA0MDIyMjA2NTVaFw0yNTA0MDMxMDA2NTVaMAAwWTATBgcqhkjOPQIB
+      BggqhkjOPQMBBwNCAATsE18tx6sRq0o20UR5JFr4xjx1wij5TqUVJneiE8mb6uKR
+      6gUZ9ZYU1CjuJzlzJb8uKrC3s243EfUH1yM7/VE2o4GNMIGKMA4GA1UdDwEB/wQE
+      AwIHgDAdBgNVHSUEFjAUBggrBgEFBQcDAQYIKwYBBQUHAwIwHQYDVR0OBBYEFGEe
+      c0KBJ2GkJgvZtn7w3kXZ9ghaMB8GA1UdIwQYMBaAFA2XKRKTqZTxAvBc/sh4EoW/
+      ji3tMBkGA1UdEQEB/wQPMA2CC3NlcnZlci5ob21lMAoGCCqGSM49BAMCA0gAMEUC
+      IFghdRMEy1t2IXV00dlZIwWfn6Mdeuhm6RxiRHbQahZHAiEA4SfarpvhXGtvE/fA
+      3YbElMreWwZ9x4gtJtFhA3qrjHY=
       -----END CERTIFICATE-----
     ''
   ];
-
 }
