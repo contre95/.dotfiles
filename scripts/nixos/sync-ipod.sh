de5044d311c6d0db89be7d699c7ce928d8eb9acc
Contre <lucascontre95@gmail.com>
2025-06-04 00:43:47 +0200

    feat(ipod): Auto Sync UDEV

--- scripts/nixos/sync-ipod.sh
+++ scripts/nixos/sync-ipod.sh
@@ -1,36 +1,3 @@
-#!/nix/store/$(realpath $(which bash) | cut -d/ -f4-)/bin/bash
-
-echo hi
-#
-# DISK_UUID="a55281ac"
-# MOUNT_POINT="/mnt/ipod-classic"
-# TARGET_FOLDER="/mnt/SSD/config/emby/data/userplaylists"
-#
-# exec >> /var/log/sync-disk.log 2>&1
-# echo "[$(date)] Disk sync triggered"
-#
-# sleep 2
-#
-# DEVICE=$(blkid -U "$DISK_UUID")
-#
-# if [ -z "$DEVICE" ]; then
-#   echo "Disk not found"
-#   curl -d "Disk $DISK_UUID not found" https://ntfy.contre.io/all
-#   exit 1
-# fi
-#
-# mkdir -p "$MOUNT_POINT"
-#
-# mount "$DEVICE" "$MOUNT_POINT"
-#
-# if [ $? -ne 0 ]; then
-#   curl -d "Mount failed for $DEVICE" https://ntfy.contre.io/all
-#   exit 1
-# fi
-#
-# rsync -av --delete "$MOUNT_POINT/" "$TARGET_FOLDER"
-#
-# curl -d "Disk sync completed successfully for $DEVICE" https://ntfy.contre.io/all
-#
-# umount "$MOUNT_POINT"
-#
+#!/run/current-system/sw/bin/bash
+/home/contre/.nix-profile/bin/rsync -av --inplace --info=progress2 /mnt/SSD/config/emby/data/userplaylists/ /mnt/ipod/Playlists &&\
+/run/current-system/sw/bin/curl -d "Disk sync completed successfully for IPod" https://ntfy.contre.io/all
