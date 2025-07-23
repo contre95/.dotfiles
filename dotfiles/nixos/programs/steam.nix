75d488572757c6cbe7a25f5be721e64a02a91dae
Contre <lucascontre95@gmail.com>
2025-07-23 14:32:56 +0200

    feat: +tablet

--- dotfiles/nixos/programs/steam.nix
+++ dotfiles/nixos/programs/steam.nix
@@ -23,11 +23,12 @@ in
         ];
         programs.steam = {
           enable = true;
-          remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-          dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+          remotePlay.openFirewall = true;
+          dedicatedServer.openFirewall = true;
+          localNetworkGameTransfers.openFirewall = true;
           gamescopeSession.enable = true;
           extraCompatPackages = with pkgs; [
-            # proton-ge-bin
+            proton-ge-bin
           ];
         };
       }
