57972f678bcf58cea0c021a9168e3bf4dbfd0707
Contre <lucascontre95@gmail.com>
2025-07-30 19:06:38 +0200

    feat: waybar tablet

--- scripts/waybar-scr/mic-status.sh
+++ scripts/waybar-scr/mic-status.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if wpctl status | grep "MUTED" | grep "*" >/dev/null 2>&1; then
-  echo '<span color="#FF6363"> </span>' "$(wpctl get-volume @DEFAULT_SOURCE@ | awk -F" " '{ print $2 * 100 "%"}')"
+  echo '<span color="#FF6363"></span>' "$(wpctl get-volume @DEFAULT_SOURCE@ | awk -F" " '{ print $2 * 100 "%"}')"
 else
-  echo '<span color="#6D6D6D"> </span>' "$(wpctl get-volume @DEFAULT_SOURCE@ | awk -F" " '{ print $2 * 100 "%"}')"
+  echo '<span color="#6D6D6D"></span>' "$(wpctl get-volume @DEFAULT_SOURCE@ | awk -F" " '{ print $2 * 100 "%"}')"
 fi
 
