d6a7f58a981992c9c75e9bddfa96b390433b3089
Contre <lucascontre95@gmail.com>
2025-09-20 01:22:46 +0200

    audio

--- scripts/audio-scr/mic-loopback.sh
+++ scripts/audio-scr/mic-loopback.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 mapfile -t LOOPBACKMODULES < <(pactl list short modules | awk '$2 =="module-loopback" { print $1 }') # mapfile is only available in bash>=4
 
 if [[ ${#LOOPBACKMODULES[@]} -gt 0 ]]; then # 1 becaouse for some reason there's an empty something in the array (don't wanna bother)
